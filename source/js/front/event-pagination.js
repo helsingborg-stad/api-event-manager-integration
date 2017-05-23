bfe9500309915c056f9ff7089b82e443acb8041e
Anders Fajerson <anders@perifer.se>
2017-05-23 13:14:17 +0200

    Fix invalid attributes

--- source/js/front/event-pagination.js
+++ source/js/front/event-pagination.js
@@ -16,9 +16,9 @@ EventManagerIntegration.Event.Module = (function ($) {
     // Load pagination bar to event modules
     Module.prototype.initEventPagination = function () {
     	$(".modularity-mod-event").each(function( key, value ) {
-    		var moduleId 	= $(this).find('[module-id]').attr('module-id');
+    		var moduleId 	= $(this).find('[module-id]').attr('data-module-id');
     		var pages 	 	= $(this).find('.module-pagination').attr('data-pages');
-    		var showArrows 	= $(this).find('.module-pagination').attr('show-arrows');
+    		var showArrows 	= $(this).find('.module-pagination').attr('data-show-arrows');
     		var module   	= $(this);
 
 		    $(this).find('.module-pagination').pagination({
