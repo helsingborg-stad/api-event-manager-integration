3dd677f6df3d9608b6c2fc9179e350335feb07aa
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-05-04 10:56:46 +0200

    Insert new fields from API

--- source/php/Parser/EventManagerApi.php
+++ source/php/Parser/EventManagerApi.php
@@ -41,50 +41,56 @@ class EventManagerApi extends \EventManagerIntegration\Parser
      * Save event to database
      * @param  object $event Event data
      * @return void
+     * @throws \Exception
      */
     public function saveEvent($event)
     {
-        $post_title             = ! empty($event['title']['rendered']) ? $event['title']['rendered'] : null;
-        $post_content           = ! empty($event['content']['rendered']) ? $event['content']['rendered'] : null;
-        $featured_media         = ! empty($event['featured_media']['source_url']) ? $event['featured_media']['source_url'] : null;
-        $categories             = ! empty($event['event_categories']) ? array_map('ucwords', array_map('trim', $event['event_categories'])) : array();
-        $tags                   = ! empty($event['event_tags']) ? array_map('strtolower', array_map('trim', $event['event_tags'])) : array();
-        $groups                 = ! empty($event['user_groups']) ? $event['user_groups'] : array();
-        $occasions              = ! empty($event['occasions']) ? $event['occasions'] : null;
-        $event_link             = ! empty($event['event_link']) ? $event['event_link'] : null;
-        $additional_links       = ! empty($event['additional_links']) ? $event['additional_links'] : null;
-        $related_events         = ! empty($event['related_events']) ? $event['related_events'] : null;
-        $organizers             = ! empty($event['organizers']) ? $event['organizers'] : null;
-        $supporters             = ! empty($event['supporters']) ? $event['supporters'] : null;
-        $booking_link           = ! empty($event['booking_link']) ? $event['booking_link'] : null;
-        $booking_phone          = ! empty($event['booking_phone']) ? $event['booking_phone'] : null;
-        $age_restriction        = ! empty($event['age_restriction']) ? $event['age_restriction'] : null;
-        $membership_cards       = ! empty($event['membership_cards']) ? $event['membership_cards'] : null;
-        $price_information      = ! empty($event['price_information']) ? $event['price_information'] : null;
-        $ticket_includes        = ! empty($event['ticket_includes']) ? $event['ticket_includes'] : null;
-        $price_adult            = ! empty($event['price_adult']) ? $event['price_adult'] : null;
-        $price_children         = ! empty($event['price_children']) ? $event['price_children'] : null;
-        $children_age           = ! empty($event['children_age']) ? $event['children_age'] : null;
-        $price_student          = ! empty($event['price_student']) ? $event['price_student'] : null;
-        $price_senior           = ! empty($event['price_senior']) ? $event['price_senior'] : null;
-        $senior_age             = ! empty($event['senior_age']) ? $event['senior_age'] : null;
-        $booking_group          = ! empty($event['booking_group']) ? $event['booking_group'] : null;
-        $gallery                = ! empty($event['gallery']) ? $event['gallery'] : null;
-        $facebook               = ! empty($event['facebook']) ? $event['facebook'] : null;
-        $twitter                = ! empty($event['twitter']) ? $event['twitter'] : null;
-        $instagram              = ! empty($event['instagram']) ? $event['instagram'] : null;
-        $google_music           = ! empty($event['google_music']) ? $event['google_music'] : null;
-        $spotify                = ! empty($event['spotify']) ? $event['spotify'] : null;
-        $soundcloud             = ! empty($event['soundcloud']) ? $event['soundcloud'] : null;
-        $deezer                 = ! empty($event['deezer']) ? $event['deezer'] : null;
-        $youtube                = ! empty($event['youtube']) ? $event['youtube'] : null;
-        $vimeo                  = ! empty($event['vimeo']) ? $event['vimeo'] : null;
-        $post_status            = get_field('event_post_status', 'option') ? get_field('event_post_status', 'option') : 'publish';
-        $location               = ! empty($event['location']) ? $event['location'] : null;
-        $additional_locations   = ! empty($event['additional_locations']) ? $event['additional_locations'] : null;
-        $latitude               = is_array($location) && ! empty($location['latitude']) ? $location['latitude'] : null;
-        $longitude              = is_array($location) && ! empty($location['longitude']) ? $location['longitude'] : null;
-        $last_update            = ! empty($event['modified_gmt']) ? $event['modified_gmt'] : null;
+        $post_title                     = ! empty($event['title']['rendered']) ? $event['title']['rendered'] : null;
+        $post_content                   = ! empty($event['content']['rendered']) ? $event['content']['rendered'] : null;
+        $featured_media                 = ! empty($event['featured_media']['source_url']) ? $event['featured_media']['source_url'] : null;
+        $categories                     = ! empty($event['event_categories']) ? array_map('ucwords', array_map('trim', $event['event_categories'])) : array();
+        $tags                           = ! empty($event['event_tags']) ? array_map('strtolower', array_map('trim', $event['event_tags'])) : array();
+        $groups                         = ! empty($event['user_groups']) ? $event['user_groups'] : array();
+        $occasions                      = ! empty($event['occasions']) ? $event['occasions'] : null;
+        $event_link                     = ! empty($event['event_link']) ? $event['event_link'] : null;
+        $additional_links               = ! empty($event['additional_links']) ? $event['additional_links'] : null;
+        $related_events                 = ! empty($event['related_events']) ? $event['related_events'] : null;
+        $organizers                     = ! empty($event['organizers']) ? $event['organizers'] : null;
+        $supporters                     = ! empty($event['supporters']) ? $event['supporters'] : null;
+        $booking_link                   = ! empty($event['booking_link']) ? $event['booking_link'] : null;
+        $booking_phone                  = ! empty($event['booking_phone']) ? $event['booking_phone'] : null;
+        $age_restriction                = ! empty($event['age_restriction']) ? $event['age_restriction'] : null;
+        $membership_cards               = ! empty($event['membership_cards']) ? $event['membership_cards'] : null;
+        $price_information              = ! empty($event['price_information']) ? $event['price_information'] : null;
+        $ticket_includes                = ! empty($event['ticket_includes']) ? $event['ticket_includes'] : null;
+        $price_adult                    = ! empty($event['price_adult']) ? $event['price_adult'] : null;
+        $price_children                 = ! empty($event['price_children']) ? $event['price_children'] : null;
+        $children_age                   = ! empty($event['children_age']) ? $event['children_age'] : null;
+        $price_student                  = ! empty($event['price_student']) ? $event['price_student'] : null;
+        $price_senior                   = ! empty($event['price_senior']) ? $event['price_senior'] : null;
+        $senior_age                     = ! empty($event['senior_age']) ? $event['senior_age'] : null;
+        $booking_group                  = ! empty($event['booking_group']) ? $event['booking_group'] : null;
+        $gallery                        = ! empty($event['gallery']) ? $event['gallery'] : null;
+        $facebook                       = ! empty($event['facebook']) ? $event['facebook'] : null;
+        $twitter                        = ! empty($event['twitter']) ? $event['twitter'] : null;
+        $instagram                      = ! empty($event['instagram']) ? $event['instagram'] : null;
+        $google_music                   = ! empty($event['google_music']) ? $event['google_music'] : null;
+        $spotify                        = ! empty($event['spotify']) ? $event['spotify'] : null;
+        $soundcloud                     = ! empty($event['soundcloud']) ? $event['soundcloud'] : null;
+        $deezer                         = ! empty($event['deezer']) ? $event['deezer'] : null;
+        $youtube                        = ! empty($event['youtube']) ? $event['youtube'] : null;
+        $vimeo                          = ! empty($event['vimeo']) ? $event['vimeo'] : null;
+        $post_status                    = get_field('event_post_status', 'option') ? get_field('event_post_status', 'option') : 'publish';
+        $location                       = ! empty($event['location']) ? $event['location'] : null;
+        $additional_locations           = ! empty($event['additional_locations']) ? $event['additional_locations'] : null;
+        $latitude                       = is_array($location) && ! empty($location['latitude']) ? $location['latitude'] : null;
+        $longitude                      = is_array($location) && ! empty($location['longitude']) ? $location['longitude'] : null;
+        $last_update                    = ! empty($event['modified_gmt']) ? $event['modified_gmt'] : null;
+        $ticket_stock                   = ! empty($event['ticket_stock']) ? $event['ticket_stock'] : null;
+        $tickets_remaining              = ! empty($event['tickets_remaining']) ? $event['tickets_remaining'] : null;
+        $additional_ticket_retailers    = ! empty($event['additional_ticket_retailers']) ? $event['additional_ticket_retailers'] : null;
+        $additional_ticket_types        = ! empty($event['additional_ticket_types']) ? $event['additional_ticket_types'] : null;
+        $price_range                    = ! empty($event['price_range']) ? $event['price_range'] : null;
 
         $pass_tax_filter = $this->checkFilters($this->filterTaxonomies($categories, 0), $this->filterTaxonomies($tags, 1));
         $passes = true;
@@ -118,53 +124,63 @@ class EventManagerApi extends \EventManagerIntegration\Parser
 
         // Save event if it passed taxonomy and group filters
         if ($passes) {
-            $event = new Event(
-                array(
-                'post_title'            => $post_title,
-                'post_content'          => $post_content,
-                'post_status'           => $post_status,
-                ),
-                array(
-                '_event_manager_id'     => $event['id'],
-                'categories'            => $categories,
-                'tags'                  => $tags,
-                'groups'                => $groups,
-                'occasions_complete'    => $occasions,
-                'event_link'            => $event_link,
-                'additional_links'      => $additional_links,
-                'related_events'        => $related_events,
-                'location'              => $location,
-                'latitude'              => $latitude,
-                'longitude'             => $longitude,
-                'additional_locations'  => $additional_locations,
-                'organizers'            => $organizers,
-                'supporters'            => $supporters,
-                'booking_link'          => $booking_link,
-                'booking_phone'         => $booking_phone,
-                'age_restriction'       => $age_restriction,
-                'membership_cards'      => $membership_cards,
-                'price_information'     => $price_information,
-                'ticket_includes'       => $ticket_includes,
-                'price_adult'           => $price_adult,
-                'price_children'        => $price_children,
-                'children_age'          => $children_age,
-                'price_student'         => $price_student,
-                'price_senior'          => $price_senior,
-                'senior_age'            => $senior_age,
-                'booking_group'         => $booking_group,
-                'gallery'               => $gallery,
-                'facebook'              => $facebook,
-                'twitter'               => $twitter,
-                'instagram'             => $instagram,
-                'google_music'          => $google_music,
-                'spotify'               => $spotify,
-                'soundcloud'            => $soundcloud,
-                'deezer'                => $deezer,
-                'youtube'               => $youtube,
-                'vimeo'                 => $vimeo,
-                'last_update'           => $last_update,
-                )
-            );
+            try {
+                $event = new Event(
+                    array(
+                        'post_title' => $post_title,
+                        'post_content' => $post_content,
+                        'post_status' => $post_status,
+                    ),
+                    array(
+                        '_event_manager_id' => $event['id'],
+                        'categories' => $categories,
+                        'tags' => $tags,
+                        'groups' => $groups,
+                        'occasions_complete' => $occasions,
+                        'event_link' => $event_link,
+                        'additional_links' => $additional_links,
+                        'related_events' => $related_events,
+                        'location' => $location,
+                        'latitude' => $latitude,
+                        'longitude' => $longitude,
+                        'additional_locations' => $additional_locations,
+                        'organizers' => $organizers,
+                        'supporters' => $supporters,
+                        'booking_link' => $booking_link,
+                        'booking_phone' => $booking_phone,
+                        'age_restriction' => $age_restriction,
+                        'membership_cards' => $membership_cards,
+                        'price_information' => $price_information,
+                        'ticket_includes' => $ticket_includes,
+                        'price_adult' => $price_adult,
+                        'price_children' => $price_children,
+                        'children_age' => $children_age,
+                        'price_student' => $price_student,
+                        'price_senior' => $price_senior,
+                        'senior_age' => $senior_age,
+                        'booking_group' => $booking_group,
+                        'gallery' => $gallery,
+                        'facebook' => $facebook,
+                        'twitter' => $twitter,
+                        'instagram' => $instagram,
+                        'google_music' => $google_music,
+                        'spotify' => $spotify,
+                        'soundcloud' => $soundcloud,
+                        'deezer' => $deezer,
+                        'youtube' => $youtube,
+                        'vimeo' => $vimeo,
+                        'last_update' => $last_update,
+                        'ticket_stock' => $ticket_stock,
+                        'tickets_remaining' => $tickets_remaining,
+                        'additional_ticket_retailers' => $additional_ticket_retailers,
+                        'additional_ticket_types' => $additional_ticket_types,
+                        'price_range' => $price_range
+                    )
+                );
+            } catch (\Exception $e) {
+                error_log(print_r($e, true));
+                return;
+            }
             $createSuccess = $event->save();
 
             if ($createSuccess && ! empty($featured_media)) {
