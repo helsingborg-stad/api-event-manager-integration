d2b210242ef3ec33d26e563b068107aea6cdf834
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-06-13 11:08:00 +0200

    Limits the draw map to one polygon

--- dist/js/event-integration-admin.dev.js
+++ dist/js/event-integration-admin.dev.js
@@ -66,44 +66,79 @@ EventManagerIntegration.Admin = EventManagerIntegration.Admin || {};
 
 EventManagerIntegration.Admin.DrawMap = (function ($) {
 
-    function DrawMap() {
-        $(function () {
-            if (typeof google === 'object' && typeof google.maps === 'object' && pagenow === 'event_page_acf-options-options') {
-                this.init();
-            }
-        }.bind(this));
-    }
+        var drawingManager;
 
-    DrawMap.prototype.init = function () {
-        var drawDiv = document.getElementById('draw-map-container');
-        if (!drawDiv) {
-            return;
+        function DrawMap() {
+            $(function () {
+                if (typeof google === 'object' && typeof google.maps === 'object' && pagenow === 'event_page_acf-options-options') {
+                    this.init();
+                }
+            }.bind(this));
         }
 
-        var mapElement = document.createElement('div');
-        mapElement.id = 'draw-map-area';
-        drawDiv.appendChild(mapElement);
-
-        var map = new google.maps.Map(mapElement, {
-            zoom: 11,
-            center: {lat: 56.04673, lng: 12.69437}
-        });
-
-        var drawingManager = new google.maps.drawing.DrawingManager({
-            drawingMode: google.maps.drawing.OverlayType.POLYGON,
-            drawingControl: true,
-            drawingControlOptions: {
-                position: google.maps.ControlPosition.TOP_CENTER,
-                drawingModes: ['polygon']
+        DrawMap.prototype.init = function () {
+            var drawDiv = document.getElementById('draw-map-container');
+            if (!drawDiv) {
+                return;
             }
-        });
-        drawingManager.setMap(map);
 
-    };
+            var mapElement = document.createElement('div');
+            mapElement.id = 'draw-map-area';
+            drawDiv.appendChild(mapElement);
 
-    return new DrawMap();
+            var map = new google.maps.Map(mapElement, {
+                zoom: 13,
+                center: {lat: 56.04673, lng: 12.69437},
+                disableDefaultUI: true,
+                zoomControl: true
+            });
 
-})(jQuery);
+            var polyOptions = {
+                strokeWeight: 0,
+                fillOpacity: 0.45,
+                fillColor: '#1e90ff'
+            };
+
+            drawingManager = new google.maps.drawing.DrawingManager({
+                map: map,
+                drawingMode: google.maps.drawing.OverlayType.POLYGON,
+                drawingControl: true,
+                drawingControlOptions: {
+                    position: google.maps.ControlPosition.TOP_CENTER,
+                    drawingModes: ['polygon']
+                },
+                markerOptions: {
+                    draggable: true
+                },
+                polygonOptions: polyOptions
+            });
+
+            // Fire action when polygon shape is completed
+            drawingManager.addListener('overlaycomplete', this.polygonComplete);
+        };
+
+        DrawMap.prototype.polygonComplete = function (event) {
+            var vertices = event.overlay.getPath();
+            var coords = [];
+
+            for (var i = 0; i < vertices.length; i++) {
+                var xy = vertices.getAt(i);
+                coords.push({latitude: xy.lat(), longitude: xy.lng()});
+            }
+
+            if (event.type != google.maps.drawing.OverlayType.MARKER) {
+                // Switch back to non-drawing mode after drawing a shape.
+                drawingManager.setDrawingMode(null);
+                // Hide controls
+                drawingManager.setOptions({
+                    drawingControl: false
+                });
+            }
+        };
+
+        return new DrawMap();
+    }
+)(jQuery);
 
 var EventManagerIntegration = EventManagerIntegration || {};
 EventManagerIntegration.loading = false;
