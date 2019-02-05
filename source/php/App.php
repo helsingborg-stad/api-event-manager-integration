ab125972f599416f3e7dc58b256be7e220a2c9ed
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-02-05 16:26:10 +0100

    Typo fix

--- source/php/App.php
+++ source/php/App.php
@@ -119,7 +119,7 @@ class App
      * @param  string $end_date   occasion end date
      * @return string             formatted date
      */
-    public static function formatEventDate(int $start_date, $end_date)
+    public static function formatEventDate($start_date, $end_date)
     {
         $start = date('Y-m-d H:i:s', strtotime($start_date));
         $end = date('Y-m-d H:i:s', strtotime($end_date));
