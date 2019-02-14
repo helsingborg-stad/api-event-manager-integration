5f102cb5b6c622146dc2ae76f13bf59dd66418ef
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-02-14 16:54:43 +0100

    Simple card view

--- source/js/Module/Event/Components/Card.js
+++ source/js/Module/Event/Components/Card.js
@@ -1,6 +1,17 @@
-const Cards = ({ items }) =>
-    items.map(item => {
-        console.log(item);
-    });
+const Card = ({ event }) => (
+    <a className="c-card c-card--action u-text-center" href="">
+        <img className="c-card__image" src="" alt="" />
 
-export default Cards;
+        <div className="c-card__body">
+            <div className="c-card__time u-mb-2">
+                <time>2019-01-01</time>
+            </div>
+
+            <h4 className="c-card__title">{event.post_title}</h4>
+
+            <span className="c-card__sub o-text-small">Location here</span>
+        </div>
+    </a>
+);
+
+export default Card;
