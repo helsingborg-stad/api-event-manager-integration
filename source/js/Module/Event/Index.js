f4917131be3ba10866ab919b55ce77464c1fa0e1
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-02-14 16:55:11 +0100

    Declared props

--- source/js/Module/Event/Index.js
+++ source/js/Module/Event/Index.js
@@ -1,11 +1,22 @@
-import Event from './Containers/Event';
+import Event from './Containers/Events';
 
 const domElements = document.getElementsByClassName('modularity-event-index');
 const translation = modEvent;
 
 for (let i = 0; i < domElements.length; i++) {
     const element = domElements[i];
-    const {} = element.dataset;
+    const { moduleId, settings, restUrl, gridColumn, archiveUrl, endDate } = element.dataset;
 
-    ReactDOM.render(<Event translation={translation} perPage={10} />, element);
+    ReactDOM.render(
+        <Event
+            moduleId={moduleId}
+            settings={JSON.parse(settings)}
+            translation={translation}
+            gridColumn={gridColumn}
+            restUrl={restUrl}
+            archiveUrl={archiveUrl}
+            endDate={endDate}
+        />,
+        element
+    );
 }
