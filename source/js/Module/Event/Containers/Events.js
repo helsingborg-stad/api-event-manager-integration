15b1a6648281797d1b1a332d337128a5f74baed4
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-02-14 16:57:31 +0100

    Fixed render and fetch events

--- source/js/Module/Event/Containers/Events.js
+++ source/js/Module/Event/Containers/Events.js
@@ -1,7 +1,8 @@
-import { getEvents } from '../../../Api/events';
-import Card from '../Components/Card';
+import uuidv1 from 'uuid/v1';
 import { Pagination, PreLoader, Notice } from 'hbg-react';
 import update from 'immutability-helper';
+import Card from '../Components/Card';
+import { getEvents } from '../../../Api/events';
 
 class Event extends React.Component {
     constructor() {
@@ -18,15 +19,13 @@ class Event extends React.Component {
     }
 
     getEvents = () => {
-        const { translation } = this.props;
+        const { translation, restUrl, moduleId, settings, endDate } = this.props;
+        const perPage = settings.mod_event_pagination ? settings.mod_event_per_page : -1;
+        const url = `${restUrl}wp/v2/event/module?end_date=${endDate}&per_page=${perPage}&page=1&module_id=${moduleId}`;
 
-        const restUrl =
-            'https://single.local/wp-json/wp/v2/event/module?end_date=2019-12-30&per_page=20&page=1';
-
-        getEvents(restUrl)
+        getEvents(url)
             .then(response => {
                 console.log(response);
-
                 this.setState({
                     isLoaded: true,
                     items: response,
@@ -40,31 +39,42 @@ class Event extends React.Component {
 
     render() {
         const { error, isLoaded, items } = this.state;
-        const { translation } = this.props;
+        const { settings, translation, gridColumn, archiveUrl } = this.props;
 
         if (error) {
             return (
-                <div className="u-p-2">
-                    <Notice type="warning" icon>
-                        error message
-                    </Notice>
-                </div>
+                <Notice type="warning" icon>
+                    TODO error message here
+                </Notice>
             );
         }
 
         if (!isLoaded) {
             return (
-                <div className="gutter gutter-xl">
+                <div className="gutter">
                     <PreLoader />
                 </div>
             );
         }
 
-        return items.map(event => (
-            <div className="grid-xs-12 grid-md-4 u-flex" key={event.ID}>
-                <Card event={event} />
+        return (
+            <div>
+                <div className="grid grid--columns">
+                    {items.map(event => (
+                        <div className={`u-flex ${gridColumn}`} key={uuidv1()}>
+                            <Card event={event} />
+                        </div>
+                    ))}
+                </div>
+                {settings.mod_event_archive && (
+                    <div className="grid-xs-12 u-text-center">
+                        <a className="btn btn-primary" href={archiveUrl}>
+                            {translation.moreEvents}
+                        </a>
+                    </div>
+                )}
             </div>
-        ));
+        );
     }
 }
 
