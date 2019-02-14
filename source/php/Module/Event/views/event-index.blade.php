956bbdf4969716504843e53c5ba1a6d1866c937e
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-02-14 16:51:17 +0100

    Add data as div attributes

--- source/php/Module/Event/views/event-index.blade.php
+++ source/php/Module/Event/views/event-index.blade.php
@@ -1,4 +1,4 @@
-<div class="{{ $classes }}">
+<div class="grid {{ $classes }}">
     @if (!$hideTitle && !empty($post_title))
         <div class="grid-xs-12 u-mb-4">
             <h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
@@ -6,19 +6,14 @@
     @endif
 
     <div class="grid-xs-12">
-        <div class="grid grid--columns">
-            <div class="modularity-event-{{ $template }}"
-                 data-archive-url="{{ $archive_url }}"
-            >
-                <div class="gutter gutter-xl">
-                    <div class="loading">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                </div>
-            </div>
+        <div class="modularity-event-{{ $template }}"
+             data-module-id="{{ $ID }}"
+             data-settings="{{ json_encode($settings) }}"
+             data-grid-column="{{ $gridColumn }}"
+             data-archive-url="{{ $archive_url }}"
+             data-rest-url="{{ $rest_url }}"
+             data-end-date="{{ $end_date }}"
+        >
         </div>
     </div>
 </div>
