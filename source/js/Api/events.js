b58d52e6037ef7d23cef667196e9a789a352d325
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-02-19 09:24:49 +0100

    Append params to url

--- source/js/Api/events.js
+++ source/js/Api/events.js
@@ -1,5 +1,9 @@
-const getEvents = (restUrl, nonce) =>
-    fetch(restUrl, {
+const getEvents = (url, params) => {
+    const apiUrl = new URL(url);
+    // Append params to url
+    Object.keys(params).forEach(key => apiUrl.searchParams.append(key, params[key]));
+
+    return fetch(apiUrl, {
         credentials: 'include',
         method: 'GET',
         headers: {
@@ -13,5 +17,6 @@ const getEvents = (restUrl, nonce) =>
             throw Error(response.statusText);
         })
         .then(response => response);
+};
 
 export { getEvents };
