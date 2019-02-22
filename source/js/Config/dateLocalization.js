e8c26f3b9a7a8d20fb44f9a8983902e55db7e3f0
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-02-22 16:21:21 +0100

    Date localization config file

---
+++ source/js/Config/dateLocalization.js
@@ -0,0 +1,22 @@
+const months = {
+    se: [
+        'Januari',
+        'Februari',
+        'Mars',
+        'April',
+        'Maj',
+        'Juni',
+        'Juli',
+        'Augusti',
+        'September',
+        'Oktober',
+        'November',
+        'December',
+    ],
+};
+const weekdaysLong = {
+    se: ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag'],
+};
+const weekdaysShort = { se: ['Sö', 'Må', 'Ti', 'On', 'To', 'Fr', 'Lö'] };
+
+export { months, weekdaysLong, weekdaysShort };
