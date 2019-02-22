fcb4018222f3efe62974968e78a8e5bc12c2518f
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-02-22 12:57:33 +0100

    Added date filter logic

--- source/js/Module/Event/Containers/Events.js
+++ source/js/Module/Event/Containers/Events.js
@@ -1,12 +1,12 @@
 import uuidv1 from 'uuid/v1';
 import { Pagination, PreLoader, Notice } from 'hbg-react';
 import Card from '../Components/Card';
-import SearchForm from '../Components/SearchForm';
+import Filters from '../Components/Filters';
 import { getEvents } from '../../../Api/events';
 
 class Event extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             error: null,
             isLoaded: false,
@@ -14,6 +14,8 @@ class Event extends React.Component {
             currentPage: 1,
             totalPages: 1,
             searchString: '',
+            startDate: props.startDate,
+            endDate: props.endDate,
         };
     }
 
@@ -21,16 +23,18 @@ class Event extends React.Component {
         this.getEvents();
     }
 
+    /**
+     * Fetch events from API endpoint
+     */
     getEvents = () => {
         this.setState({ isLoaded: false, error: null });
-
-        const { currentPage, searchString } = this.state;
+        // Declare states and props
+        const { currentPage, searchString, startDate, endDate } = this.state;
         const {
             translation,
             restUrl,
             moduleId,
             settings,
-            endDate,
             lat,
             lng,
             distance,
@@ -39,9 +43,13 @@ class Event extends React.Component {
             groups,
         } = this.props;
         const perPage = settings.mod_event_pagination ? settings.mod_event_per_page : -1;
+        // Concatenate all taxonomies together
         const taxonomies = categories.concat(tags, groups);
+        // The API base url
         const url = `${restUrl}wp/v2/event/module`;
+        // Create list of filterable parameters
         const params = {
+            start_date: startDate,
             end_date: endDate,
             per_page: perPage,
             page: currentPage,
@@ -53,6 +61,12 @@ class Event extends React.Component {
             search_string: searchString,
         };
 
+        console.log('Start');
+        console.log(startDate);
+        console.log('End');
+        console.log(endDate);
+
+        // Fetch events
         getEvents(url, params)
             .then(response => {
                 this.setState({
@@ -72,6 +86,9 @@ class Event extends React.Component {
             });
     };
 
+    /**
+     * Pagination next page handler
+     */
     nextPage = () => {
         let { currentPage, totalPages } = this.state;
         if (currentPage === totalPages) {
@@ -81,6 +98,9 @@ class Event extends React.Component {
         this.setState({ currentPage }, () => this.getEvents());
     };
 
+    /**
+     * Pagination previous page handler
+     */
     prevPage = () => {
         let { currentPage } = this.state;
         if (currentPage <= 1) {
@@ -90,6 +110,10 @@ class Event extends React.Component {
         this.setState({ currentPage }, () => this.getEvents());
     };
 
+    /**
+     * Pagination input page number handler
+     * @param e
+     */
     paginationInput = e => {
         const { totalPages } = this.state;
         let currentPage = e.target.value ? parseInt(e.target.value) : '';
@@ -102,15 +126,51 @@ class Event extends React.Component {
         });
     };
 
+    /**
+     * Search string change handler
+     * @param e
+     */
     updateSearchString = e => {
         this.setState({
             searchString: e.target.value,
         });
     };
 
-    onSearchSubmit = e => {
+    /**
+     * Submit form handler
+     * @param e
+     */
+    onSubmit = e => {
         e.preventDefault();
         this.setState({ currentPage: 1 }, () => this.getEvents());
+    };
+
+    /**
+     * From date change handler
+     * @param date
+     */
+    fromDateChange = date => {
+        console.log('Change from date');
+        this.setState({ startDate: this.formatDate(date) });
+    };
+
+    /**
+     * To date change handler
+     * @param date
+     */
+    toDateChange = date => {
+        console.log('Change To date');
+        this.setState({ endDate: this.formatDate(date) });
+    };
+
+    /**
+     * Format date (Y-m-d)
+     * @param date
+     * @returns {string}
+     */
+    formatDate = date => {
+        const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+        return date.toLocaleDateString('sv-SE', options);
     };
 
     render() {
@@ -119,14 +179,15 @@ class Event extends React.Component {
 
         return (
             <div>
-                <div className="grid u-mb-3">
-                    <div className="grid-xs-12 grid-md-auto u-mb-2 u-mb-0@md u-mb-0@lg u-mb-0@xl">
-                        <SearchForm
-                            translation={translation}
-                            searchString={this.updateSearchString}
-                            searchSubmit={this.onSearchSubmit}
-                        />
-                    </div>
+                <div className="u-mb-3">
+                    <Filters
+                        translation={translation}
+                        updateSearchString={this.updateSearchString}
+                        onSubmit={this.onSubmit}
+                        fromDateChange={this.fromDateChange}
+                        toDateChange={this.toDateChange}
+                        formatDate={this.formatDate}
+                    />
                 </div>
 
                 {!isLoaded && (
