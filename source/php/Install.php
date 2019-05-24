6d7e96299bffb232c16d2d003602422f6c76e287
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-05-24 11:26:01 +0200

    Class for creating/updating database tables

---
+++ source/php/Install.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace EventManagerIntegration;
+
+global $eventDatabaseVersion;
+$eventDatabaseVersion = '1.1';
+
+/**
+ * Creates/updates necessary database tables
+ */
+
+class Install
+{
+    public function __construct()
+    {
+        // Use 'updateDbCheck' method when db table needs to be updated
+        add_action('plugins_loaded', array($this, 'updateDbCheck'));
+    }
+
+    /**
+     * Check if db needs to be updated
+     * @return void
+     */
+    public function updateDbCheck()
+    {
+        error_log("Do db checkup");
+        global $eventDatabaseVersion;
+        if (version_compare(get_site_option('event_manager_integration_version'), $eventDatabaseVersion) < 0) {
+            error_log("Not latest version, DO update/install");
+
+            $this->createTables();
+        }
+    }
+
+    /**
+     * Creates the event occasions db table
+     * @return void
+     */
+    public static function createTables()
+    {
+        error_log("do update/install");
+
+        global $wpdb;
+        global $eventDatabaseVersion;
+
+        $charsetCollate = $wpdb->get_charset_collate();
+        $basePrefix = $wpdb->base_prefix;
+
+        $tableName = $basePrefix . 'integrate_occasions';
+        $sql = "CREATE TABLE $tableName (
+        ID BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+        event_id BIGINT(20) UNSIGNED NOT NULL,
+        start_date DATETIME NOT NULL,
+        end_date DATETIME NOT NULL,
+        door_time DATETIME DEFAULT NULL,
+        status VARCHAR(50) DEFAULT NULL,
+        exception_information VARCHAR(400) DEFAULT NULL,
+        content_mode VARCHAR(50) DEFAULT NULL,
+        content LONGTEXT DEFAULT NULL,
+        location_mode VARCHAR(50) DEFAULT NULL,
+        location LONGTEXT DEFAULT NULL,
+        PRIMARY KEY  (ID)
+        ) $charsetCollate;";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+
+        update_site_option('event_manager_integration_version', $eventDatabaseVersion);
+    }
+}
