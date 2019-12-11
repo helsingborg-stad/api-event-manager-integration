a1b2f7c6dc17c4c05049e2228a33dd6c0774faa6
Jonatan Hanson <jonatan.hanson@gmail.com>
2019-12-11 10:51:30 +0100

    Added value prop

--- source/js/Module/Event/Components/SearchBar.js
+++ source/js/Module/Event/Components/SearchBar.js
@@ -1,21 +1,22 @@
-const SearchBar = ({ translation, updateSearchString }) => (
-    <div>
-        <label htmlFor="filter-keyword" className="text-sm sr-only">
-            <strong>{translation.search}</strong>
-        </label>
+const SearchBar = ({ searchString, translation, updateSearchString }) => (
+  <div>
+    <label htmlFor="filter-keyword" className="text-sm sr-only">
+      <strong>{translation.search}</strong>
+    </label>
 
-        <div className="input-group">
-            <span className="input-group-addon">
-                <i className="fa fa-search" />
-            </span>
-            <input
-                type="text"
-                id="filter-keyword"
-                className="form-control"
-                onChange={updateSearchString}
-                placeholder={translation.search}
-            />
-        </div>
+    <div className="input-group">
+      <span className="input-group-addon">
+        <i className="fa fa-search" />
+      </span>
+      <input
+        value={searchString}
+        type="text"
+        id="filter-keyword"
+        className="form-control"
+        onChange={updateSearchString}
+        placeholder={translation.search}
+      />
     </div>
+  </div>
 );
 export default SearchBar;
