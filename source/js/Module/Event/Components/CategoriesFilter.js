95bc0d1c8d3646a19f18ac9a6ac7258435778479
Jonatan Hanson <jonatan.hanson@gmail.com>
2019-12-12 10:05:11 +0100

    Increased width

--- source/js/Module/Event/Components/CategoriesFilter.js
+++ source/js/Module/Event/Components/CategoriesFilter.js
@@ -8,7 +8,7 @@ const CategoriesFilter = ({ categories, onCategoryChange, title }) => (
 
         <Dropdown title={title} toggleClass="btn" id="filter-categories">
             {categories.map(item => (
-              <div style={{ maxWidth: '220px', width: 'max-content' }}>
+              <div style={{ maxWidth: '250px', width: 'max-content' }}>
                 <label key={item.id} className="checkbox u-px-1">
                     <input
                         type="checkbox"
