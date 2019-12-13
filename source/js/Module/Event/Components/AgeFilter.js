6c473c4cff906915637a3fdbe838b2b62c918b4d
Jonatan Hanson <jonatan.hanson@gmail.com>
2019-12-13 14:33:12 +0100

    Indent file

--- source/js/Module/Event/Components/AgeFilter.js
+++ source/js/Module/Event/Components/AgeFilter.js
@@ -1,23 +1,23 @@
 import { Dropdown } from 'hbg-react';
 
 const AgeFilter = ({ translation, ageRange, onAgeChange }) => (
-    <div>
-        <label htmlFor="filter-categories" className="text-sm sr-only">
-            {translation.selectAge}
+  <div>
+    <label htmlFor="filter-categories" className="text-sm sr-only">
+      {translation.selectAge}
+    </label>
+    <Dropdown title={translation.selectAge} toggleClass="btn">
+      {ageRange.map(item => (
+        <label key={item.value} className="checkbox u-px-1">
+          <input
+            type="checkbox"
+            value={item.value}
+            onChange={e => onAgeChange(e, item.value)}
+            checked={item.checked}
+          />{' '}
+          {item.value} {translation.years}
         </label>
-        <Dropdown title={translation.selectAge} toggleClass="btn">
-            {ageRange.map(item => (
-                <label key={item.value} className="checkbox u-px-1">
-                    <input
-                        type="checkbox"
-                        value={item.value}
-                        onChange={e => onAgeChange(e, item.value)}
-                        checked={item.checked}
-                    />{' '}
-                    {item.value} {translation.years}
-                </label>
-            ))}
-        </Dropdown>
-    </div>
+      ))}
+    </Dropdown>
+  </div>
 );
 export default AgeFilter;
