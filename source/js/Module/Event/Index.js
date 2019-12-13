7de367f0764f1a1c61c4f5b643c2e3e3f8a9395a
Jonatan Hanson <jonatan.hanson@gmail.com>
2019-12-13 14:32:45 +0100

    Indent file

--- source/js/Module/Event/Index.js
+++ source/js/Module/Event/Index.js
@@ -8,19 +8,19 @@ const domElements = document.getElementsByClassName('modularity-event-index');
 const translation = modEvent;
 
 for (let i = 0; i < domElements.length; i++) {
-    const element = domElements[i];
-    const { settings, categories, tags, groups, ageRange } = element.dataset;
+  const element = domElements[i];
+  const { settings, categories, tags, groups, ageRange } = element.dataset;
 
-    ReactDOM.render(
-        <FilterableEventsContainer
-            {...element.dataset}
-            translation={translation}
-            settings={JSON.parse(settings)}
-            categories={JSON.parse(categories)}
-            tags={JSON.parse(tags)}
-            groups={JSON.parse(groups)}
-            ageRange={JSON.parse(ageRange)}
-        />,
-        element
-    );
+  ReactDOM.render(
+    <FilterableEventsContainer
+      {...element.dataset}
+      translation={translation}
+      settings={JSON.parse(settings)}
+      categories={JSON.parse(categories)}
+      tags={JSON.parse(tags)}
+      groups={JSON.parse(groups)}
+      ageRange={JSON.parse(ageRange)}
+    />,
+    element
+  );
 }
