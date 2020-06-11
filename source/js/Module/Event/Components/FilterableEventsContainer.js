150ce9b7b6b1f0b564bc164137ca07dd06976663
Jacob Winding <jacobkwinding@gmail.com>
2020-06-11 16:30:22 +0200

    Fix translation bug with hgb.se/lov

--- source/js/Module/Event/Components/FilterableEventsContainer.js
+++ source/js/Module/Event/Components/FilterableEventsContainer.js
@@ -39,7 +39,6 @@ class FilterableEventsContainer extends React.Component {
 
     // Collect query string params
     const urlParams = new URLSearchParams(window.location.search);
-
     // Remove empty values and map param keys with values
     const parameterValues = availableQueryStringParams
       .filter(({ param }) => urlParams.get(param))
@@ -47,7 +46,7 @@ class FilterableEventsContainer extends React.Component {
         let value;
         switch (type) {
           case 'int':
-            value = parseInt(urlParams.get(param));
+            value = parseInt(urlParams.get(param));           
             break;
           case 'array':
             // Get all parameter values
@@ -64,6 +63,7 @@ class FilterableEventsContainer extends React.Component {
           value = urlParams.get(param);
         }
 
+
         return { param, type, value };
       });
 
@@ -77,6 +77,19 @@ class FilterableEventsContainer extends React.Component {
     this.setState({ ...stateObj }, () => this.getEvents());
   };
 
+   loadQueryString = () => { 
+    let parameters = {}; 
+    let searchString = location.search.substr(1); 
+    let pairs = searchString.split("&"); 
+    let parts;
+    for(let i = 0; i < pairs.length; i++){
+        parts = pairs[i].split("=");
+        let name = parts[0];
+        let data = decodeURI(parts[1]);
+        parameters[name] = data;
+    }    
+    return parameters;
+  }
   /**
    * Push state values to query string
    */
@@ -94,7 +107,7 @@ class FilterableEventsContainer extends React.Component {
     const categoryIds = this.getTaxonomyIds(categories);
     const tagIds = this.getTaxonomyIds(tags);
     const ageRangeIds = this.getTaxonomyIds(ageRange);
-
+    const params = this.loadQueryString();
     // Set query parameters
     setQuery(
       {
@@ -108,6 +121,10 @@ class FilterableEventsContainer extends React.Component {
       },
       { pushState: true }
     );
+      if(params.translate){
+        location.hash = "#translate"; 
+      }
+
   };
 
   /**
@@ -200,7 +217,7 @@ class FilterableEventsContainer extends React.Component {
       tags,
     };
 
-    // Fetch events
+    // Fetch events 
     getEvents(url, params)
       .then(response => {
         this.setState({
