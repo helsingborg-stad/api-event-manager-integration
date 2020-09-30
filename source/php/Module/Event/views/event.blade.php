a8df891f0dd6084b06236d2f116e01edb5aefe4c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-09-30 14:51:02 +0200

    Adds new classes and button to module (list)

--- source/php/Module/Event/views/event.blade.php
+++ source/php/Module/Event/views/event.blade.php
@@ -1,5 +1,5 @@
 <div class="{{ $classes }} {{ isset($font_size) ? $font_size : '' }}" data-module-id="{{ $ID }}">
-    <ul>
+    <ul class="u-unlist u-margin--0 u-padding--0">
         <div class="event-module-content">
             @include('partials.list')
         </div>
@@ -9,18 +9,22 @@
         @if ($mod_event_pagination && $pagesCount > 1)
             @pagination([
                 'list' => $paginationList, 
-                'classList' => ['u-margin__top--4'], 
+                'classList' => ['u-margin--1'], 
                 'current' => isset($_GET['paged']) ? $_GET['paged'] : 1,
                 'linkPrefix' => '?paged='
             ])
             @endpagination
         @endif
         @if ($mod_event_archive)
-            <ul class="event-module-archive">
-                <li>
-                    <a href="{{ get_post_type_archive_link('event') }}"><i class="pricon pricon-plus-o"></i> <?php _e('More events', 'event-integration'); ?></a>
-                </li>
-            </ul>
+            @button([
+                'text' =>  __('More events', 'event-integration'),
+                'color' => 'primary',
+                'style' => 'basic',
+                'link' => get_post_type_archive_link('event'),
+                'icon' => 'add',
+                'reversePositions' => true,
+            ])
+            @endbutton 
         @endif
     </div>
 </div>
