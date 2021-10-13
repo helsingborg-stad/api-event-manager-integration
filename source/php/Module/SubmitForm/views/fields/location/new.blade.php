5eebe47337744c66393e8646be8fca9c240de775
Eric Rosenborg <eric.rosenborg@hiq.se>
2021-10-13 08:49:20 +0200

    Add fields for new and excisting location

---
+++ source/php/Module/SubmitForm/views/fields/location/new.blade.php
@@ -0,0 +1,55 @@
+@field([
+    'id'   => 'location-title',
+    'type' => 'text',
+    'attributeList' => [
+        'type' => 'text',
+        'name' => $location->name . '-title',
+        'field' => 'title',
+        'autocomplete' => 'off'
+    ],
+    'label' => __('Name', 'event-integration')
+])
+@endfield
+
+@field([
+    'id'   => 'location-street-address',
+    'type' => 'text',
+    'attributeList' => [
+        'type' => 'text',
+        'name' => $location->name . '-street-address',
+        'field' => 'street_address',
+        'autocomplete' => 'off'
+    ],
+    'label' => __('Street address', 'event-integration')
+])
+@endfield
+
+@field([
+    'id'   => 'location-phone',
+    'type' => 'number',
+    'attributeList' => [
+        'type' => 'text',
+        'name' => $location->name . '-postal-code',
+        'field' => 'postal_code',
+        'autocomplete' => 'off'
+    ],
+    'label' => __('Postal code', 'event-integration')
+])
+@endfield
+
+@field([
+    'id'   => 'organizer-mail',
+    'type' => 'text',
+    'attributeList' => [
+        'type' => 'text',
+        'name' => $location->name . '-city',
+        'field' => 'city',
+        'autocomplete' => 'off'
+    ],
+    'label' => __('City', 'event-integration')
+])
+@endfield
+
+
+
+
