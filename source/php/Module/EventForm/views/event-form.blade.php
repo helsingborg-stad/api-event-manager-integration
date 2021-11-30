c7678fc45eb0731199d660ab9ee9517650f794cd
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-11-30 14:26:57 +0100

    add: view entrypoint for event form

---
+++ source/php/Module/EventForm/views/event-form.blade.php
@@ -0,0 +1,20 @@
+@card([
+    'classList' => [
+        'c-card--panel',
+        $classes
+    ]
+])
+    @if (!$hideTitle && !empty($postTitle))
+        <div class="c-card__header">
+            @typography([
+                'element' => "h4"
+            ])
+                {!! $postTitle !!}
+            @endtypography
+        </div>
+    @endif
+
+    <div class="c-card__body">
+        {{-- Form goes in here --}}
+    </div>
+@endcard
