3cfd45e905d78c1dcd317aed03dcaa69a7c32741
Freya <752642+faejr@users.noreply.github.com>
2021-12-01 13:51:26 +0100

    Enclose in field-group div

--- source/php/Module/EventForm/views/partials/event-form.blade.php
+++ source/php/Module/EventForm/views/partials/event-form.blade.php
@@ -1,37 +1,39 @@
 <form name="submit-event" class="submit-event" enctype="multipart/form-data">
     @foreach($fields as $fieldGroup)
         @if(isset($fieldGroup->fields) && is_array($fieldGroup->fields))
-            @typography([
-                'variant' => 'title'
-            ])
-                {{ $fieldGroup->label }}
-            @endtypography
+            <div class="field-group">
+                @typography([
+                    'variant' => 'title'
+                ])
+                    {{ $fieldGroup->label }}
+                @endtypography
 
-            @foreach($fieldGroup->fields as $field)
-                <div class="form-group">
-                    @typography([
-                    'variant' => 'subtitle'
-                    ])
-                        {{ $field->label }}
-                        @includeWhen($field->required, 'components.required')
-                    @endtypography
+                @foreach($fieldGroup->fields as $field)
+                    <div class="form-group">
+                        @typography([
+                        'variant' => 'subtitle'
+                        ])
+                            {{ $field->label }}
+                            @includeWhen($field->required, 'components.required')
+                        @endtypography
 
-                    @includeWhen(!$field->hidden_description, 'components.description', [
-                        'description' => $field->description
-                    ])
+                        @includeWhen(!$field->hidden_description, 'components.description', [
+                            'description' => $field->description
+                        ])
 
-                    @if(isset($field->type))
-                        @includeWhen(!$field->hidden, 'components.fields.' . $field->type->component)
-                    @else
-                        <span>
-                            {{$field->label}}
-                        </span>
-                    @endif
-                </div>
-            @endforeach
+                        @if(isset($field->type))
+                            @includeWhen(!$field->hidden, 'components.fields.' . $field->type->component)
+                        @else
+                            <span>
+                                {{$field->label}}
+                            </span>
+                        @endif
+                    </div>
+                @endforeach
+            </div>
         @else
             <div>
-                <span>not implemented</span>
+                <span><b>{{ $fieldGroup->label }}</b> not implemented</span>
             </div>
         @endif
     @endforeach
