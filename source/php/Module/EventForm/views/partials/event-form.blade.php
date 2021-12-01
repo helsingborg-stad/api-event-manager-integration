008ddbb2fad3956edb212fb8e5796f451d12cf5b
Freya <752642+faejr@users.noreply.github.com>
2021-12-01 13:19:25 +0100

    Create field groups

--- source/php/Module/EventForm/views/partials/event-form.blade.php
+++ source/php/Module/EventForm/views/partials/event-form.blade.php
@@ -1,33 +1,47 @@
 <form name="submit-event" class="submit-event" enctype="multipart/form-data">
-    @foreach($fields as $field)
-        <div class="form-group">
+    @foreach($fields as $fieldGroup)
+        @if(isset($fieldGroup->fields) && is_array($fieldGroup->fields))
             @typography([
-                'variant' => 'subtitle'
+                'variant' => 'title'
             ])
-                {{ $field->label }}
-                @includeWhen($field->required, 'components.required')
+                {{ $fieldGroup->label }}
             @endtypography
 
-            @includeWhen(!$field->hidden_description, 'components.description', [
-                'description' => $field->description
-            ])
+            @foreach($fieldGroup->fields as $field)
+                <div class="form-group">
+                    @typography([
+                    'variant' => 'subtitle'
+                    ])
+                        {{ $field->label }}
+                        @includeWhen($field->required, 'components.required')
+                    @endtypography
 
-            @if(isset($field->type))
-                @includeWhen(!$field->hidden, 'components.fields.' . $field->type->component)
-            @else
-                <span>
-                    {{$field->label}}
-                </span>
-            @endif
-        </div>
+                    @includeWhen(!$field->hidden_description, 'components.description', [
+                        'description' => $field->description
+                    ])
+
+                    @if(isset($field->type))
+                        @includeWhen(!$field->hidden, 'components.fields.' . $field->type->component)
+                    @else
+                        <span>
+                            {{$field->label}}
+                        </span>
+                    @endif
+                </div>
+            @endforeach
+        @else
+            <div>
+                <span>not implemented</span>
+            </div>
+        @endif
     @endforeach
 
     @button([
-        'text'              => __('Send', 'event-integration'),
-        'color'             => 'primary',
-        'style'             => 'filled',
-        'type'              => 'submit',
-        'attributeList'    => [
+        'text' => __('Send', 'event-integration'),
+        'color' => 'primary',
+        'style' => 'filled',
+        'type' => 'submit',
+        'attributeList' => [
             'event-submit__submit-button' => ''
         ]
     ])
