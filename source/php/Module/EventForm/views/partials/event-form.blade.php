5b5d133ec4f91342008b77d6b87c4936cf13bd5a
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2021-12-01 10:49:23 +0100

    add: form wrapper

--- source/php/Module/EventForm/views/partials/event-form.blade.php
+++ source/php/Module/EventForm/views/partials/event-form.blade.php
@@ -1,7 +1,9 @@
-@foreach($fields as $field)
-    <div>
-        <span>
-            {{$field->label}}
-        </span>
-    </div>
-@endforeach
+<form name="submit-event" class="submit-event" enctype="multipart/form-data">
+    @foreach($fields as $field)
+        <div>
+            <span>
+                {{$field->label}}
+            </span>
+        </div>
+    @endforeach
+</form>
