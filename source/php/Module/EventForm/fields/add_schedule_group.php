33890d8d9ff53cb7f8f736ecdd4be52d8e7ff584
Freya <752642+faejr@users.noreply.github.com>
2021-12-14 11:53:41 +0100

    WIP: Radio buttons

--- source/php/Module/EventForm/fields/add_schedule_group.php
+++ source/php/Module/EventForm/fields/add_schedule_group.php
@@ -17,6 +17,38 @@ return (object)[
             'hidden' => false,
             'hidden_description' => !empty($data['occasion']['hidden_description']),
             'type' => (object)[
+                'component' => 'radio',
+                'props' => [
+                    'name' => 'occurance-type',
+                    'options' => [
+                        (object)[
+                            'id' => 'single-event',
+                            'label' => __('Single occurrence', 'event-integration'),
+                            'checked' => true
+                        ],
+                        (object)[
+                            'id' => 'recurring-event',
+                            'label' => __('Recurring', 'event-integration'),
+                            'checked' => false
+                        ]
+                    ]
+                ],
+            ]
+        ],
+        'date' => (object)[
+            'name' => '',
+            'label' => !empty($data['occasion']['label']) ? $data['occasion']['label'] : __(
+                'Event occurrence',
+                'event-integration'
+            ),
+            'description' => !empty($data['occasion']['description']) ? $data['occasion']['description'] : __(
+                'Add occasions to this event. Does the event occur each week? Then add a rule for recurring events. Note that end time for the event can\'t be the same as the start time.',
+                'event-integration'
+            ),
+            'required' => true,
+            'hidden' => false,
+            'hidden_description' => !empty($data['occasion']['hidden_description']),
+            'type' => (object)[
                 'component' => 'datepicker',
                 'props' => [],
             ]
