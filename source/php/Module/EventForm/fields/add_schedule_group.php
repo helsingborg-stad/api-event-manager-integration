5a308773e541d4db4e00cb4e6edf183a6b5856a6
Freya <752642+faejr@users.noreply.github.com>
2021-12-15 13:22:03 +0100

    WIP: Date and timegit

--- source/php/Module/EventForm/fields/add_schedule_group.php
+++ source/php/Module/EventForm/fields/add_schedule_group.php
@@ -35,23 +35,111 @@ return (object)[
                 ],
             ]
         ],
-        'date' => (object)[
+        'single-event' => (object)[
             'name' => '',
-            'label' => !empty($data['occasion']['label']) ? $data['occasion']['label'] : __(
-                'Event occurrence',
-                'event-integration'
-            ),
-            'description' => !empty($data['occasion']['description']) ? $data['occasion']['description'] : __(
-                'Add occasions to this event. Does the event occur each week? Then add a rule for recurring events. Note that end time for the event can\'t be the same as the start time.',
-                'event-integration'
-            ),
-            'required' => true,
+            'label' => '',
+            'description' => '',
+            'required' => false,
             'hidden' => false,
             'hidden_description' => !empty($data['occasion']['hidden_description']),
             'type' => (object)[
-                'component' => 'datepicker',
-                'props' => [],
-            ]
+                'component' => 'group',
+                'props' => [
+                    'id' => 'single-event',
+                    'className' => 'event-occurance',
+                    'fields' => [
+                        (object)[
+                            'name' => '',
+                            'label' => __('Start date', 'event-integration'),
+                            'description' => '',
+                            'required' => true,
+                            'hidden' => false,
+                            'hidden_description' => !empty($data['occasion']['hidden_description']),
+                            'type' => (object)[
+                                'component' => 'datepicker',
+                                'props' => [
+                                    'name' => 'start_date',
+                                    'title' => __('Date', 'event-integration')
+                                ],
+                            ]
+                        ],
+                        (object)[
+                            'name' => '',
+                            'label' => __('Start time', 'event-integration'),
+                            'description' => '',
+                            'required' => true,
+                            'hidden' => false,
+                            'hidden_description' => !empty($data['occasion']['hidden_description']),
+                            'type' => (object)[
+                                'component' => 'time',
+                                'props' => [
+                                    'name' => 'start_time',
+                                    'label' => __('Start time', 'event-integration')
+                                ],
+                            ]
+                        ],
+                        (object)[
+                            'name' => '',
+                            'label' => __('End date', 'event-integration'),
+                            'description' => '',
+                            'required' => true,
+                            'hidden' => false,
+                            'hidden_description' => !empty($data['occasion']['hidden_description']),
+                            'type' => (object)[
+                                'component' => 'datepicker',
+                                'props' => [
+                                    'name' => 'end_date',
+                                    'title' => __('Date', 'event-integration')
+                                ],
+                            ]
+                        ],
+                        (object)[
+                            'name' => '',
+                            'label' => __('End time', 'event-integration'),
+                            'description' => '',
+                            'required' => true,
+                            'hidden' => false,
+                            'hidden_description' => !empty($data['occasion']['hidden_description']),
+                            'type' => (object)[
+                                'component' => 'time',
+                                'props' => [
+                                    'name' => 'end_time',
+                                    'label' => __('End time', 'event-integration')
+                                ],
+                            ]
+                        ],
+                        (object)[
+                            'name' => '',
+                            'label' => '',
+                            'description' => '',
+                            'required' => false,
+                            'hidden' => false,
+                            'hidden_description' => !empty($data['occasion']['hidden_description']),
+                            'type' => (object)[
+                                'component' => 'button',
+                                'props' => [
+                                    'text' => __('Add', 'event-integration'),
+                                    'classList' => [
+                                        'btn btn btn-primary btn-sm add-occurance'
+                                    ]
+                                ],
+                            ]
+                        ],
+                    ],
+                ]
+            ],
+            'date' => (object)[
+                'name' => '',
+                'label' => '',
+                'description' => '',
+                'required' => false,
+                'hidden' => false,
+                'hidden_description' => !empty($data['occasion']['hidden_description']),
+                'type' => (object)[
+                    'component' => 'fullform',
+                    'props' => [],
+                ]
+            ],
         ],
-    ],
+    ]
 ];
