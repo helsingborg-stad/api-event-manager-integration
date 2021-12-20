f02aa35ed2c720a657b59649cea2ce241b6d4d95
Freya <752642+faejr@users.noreply.github.com>
2021-12-20 10:44:28 +0100

    WIP: create more groups

--- source/php/Module/EventForm/views/components/fields/group.blade.php
+++ source/php/Module/EventForm/views/components/fields/group.blade.php
@@ -1,24 +1,24 @@
-<div id="{{$field->type->props['id']}}" class="{{$field->type->props['className']}}">
-    @foreach($field->type->props['fields'] as $subField)
-        <div class="form-group box">
-            @typography([
-                'variant' => 'subtitle'
-            ])
-                {{ $subField->label }}
-                @includeWhen($subField->required, 'components.required')
-            @endtypography
+<div {!! $field->type->props['id'] ? 'id="' . $field->type->props['id'] . '"' : '' !!} class="{{$field->type->props['classList']}}">
+    <div class="field-group">
+        @foreach($field->type->props['fields'] as $subField)
+            <div class="form-group box">
+                @typography([
+                    'variant' => 'subtitle'
+                ])
+                    {{ $subField->label }}
+                    @includeWhen($subField->required, 'components.required')
+                @endtypography
 
-            @includeWhen(!$subField->hidden_description, 'components.description', [
-                'description' => $subField->description
-            ])
+                @includeWhen(!$subField->hidden_description, 'components.description', [
+                    'description' => $subField->description
+                ])
 
-            @if(isset($subField->type))
-                @includeWhen(!$subField->hidden, 'components.fields.' . $subField->type->component, ['field' => $subField])
-            @else
-                <span>
-                    {{$subField->label}}
-                </span>
-            @endif
-        </div>
-    @endforeach
+                @if(isset($subField->type))
+                    @includeWhen(!$subField->hidden, 'components.fields.' . $subField->type->component, ['field' => $subField])
+                @else
+                    <span>{{$subField->label}}</span>
+                @endif
+            </div>
+        @endforeach
+    </div>
 </div>
