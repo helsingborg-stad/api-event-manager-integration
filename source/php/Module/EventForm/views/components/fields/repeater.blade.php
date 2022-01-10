ebab8147126fb86af6d369b3d5974b07fc7dac3e
Freya <752642+faejr@users.noreply.github.com>
2022-01-10 13:28:56 +0100

    feat: Basic repeat functionality

--- source/php/Module/EventForm/views/components/fields/repeater.blade.php
+++ source/php/Module/EventForm/views/components/fields/repeater.blade.php
@@ -1,15 +1,31 @@
-<div class="repeater">
-    @foreach($field['subFields'] as $subField)
-        <div @if(!empty($subField['condition']))data-condition="{{ json_encode($subField['condition'] ?? []) }}"@endif>
-            @includeFirst(
-                [
-                    'components.fields.' . $subField['type'] ?? '',
-                    'components.fields._error'
-                ],
-                [
-                    'field' => $subField
-                ]
-            )
-        </div>
-    @endforeach
+<div class="js-repeater">
+    <div class="sub-fields">
+        @foreach($field['subFields'] as $subField)
+            <div @if(!empty($subField['condition']))data-condition="{{ json_encode($subField['condition'] ?? []) }}"@endif>
+                @includeFirst(
+                    [
+                        'components.fields.' . $subField['type'] ?? '',
+                        'components.fields._error'
+                    ],
+                    [
+                        'field' => $subField
+                    ]
+                )
+            </div>
+        @endforeach
+    </div>
+    @button([
+        'text' => __('Add', 'event-integration'),
+        'color' => 'primary',
+        'style' => 'filled',
+        'classList' => ['btn-repeater-add']
+    ])
+    @endbutton
+    @button([
+        'text' => __('Remove', 'event-integration'),
+        'color' => 'default',
+        'style' => 'filled',
+        'classList' => ['btn-repeater-remove', 'u-display--none']
+    ])
+    @endbutton
 </div>
