ea65d4b3dc33bf97f6c0adc1c370c25064f38b64
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-01-11 16:14:14 +0100

    fix: forward placeholder props

--- source/php/Module/EventForm/views/components/fields/field.blade.php
+++ source/php/Module/EventForm/views/components/fields/field.blade.php
@@ -8,5 +8,6 @@
     'label' => $field['label'],
     'required' => $field['required'],
     'helperText' => $field['description'] ?? '',
+    'placeholder' => $field['placeholder'] ?? '',
 ], (array )$field['props'] ?? []))
 @endfield
