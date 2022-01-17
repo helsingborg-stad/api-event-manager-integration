68747c8bc836563185ae3b6999fb001329bc82e0
Freya <752642+faejr@users.noreply.github.com>
2022-01-17 15:44:32 +0100

    feat: validate fields using json schema

--- source/php/Module/EventForm/views/components/fields/repeater.blade.php
+++ source/php/Module/EventForm/views/components/fields/repeater.blade.php
@@ -1,26 +1,16 @@
-@php
-$addButtonLabel = __('Add', 'event-integration');
-if (!empty($field['labels']) && !empty($field['labels']['addButton'])) {
-    $addButtonLabel = $field['labels']['addButton'];
-}
-$removeButtonLabel = __('Remove', 'event-integration');
-if (!empty($field['labels']) && !empty($field['labels']['removeButton'])) {
-    $removeButtonLabel = $field['labels']['removeButton'];
-}
-@endphp
 <div id="{{ $field['name'] }}" class="js-repeater">
     <div class="sub-fields">
         @include('partials.field-loop', ['fields' => $field['subFields']])
     </div>
     @button([
-        'text' => $addButtonLabel,
+        'text' => $field['labels']['addButton'],
         'color' => 'primary',
         'style' => 'filled',
         'classList' => ['btn-repeater-add']
     ])
     @endbutton
     @button([
-        'text' => $removeButtonLabel,
+        'text' => $field['labels']['removeButton'],
         'color' => 'default',
         'style' => 'filled',
         'classList' => ['btn-repeater-remove', 'u-display--none']
