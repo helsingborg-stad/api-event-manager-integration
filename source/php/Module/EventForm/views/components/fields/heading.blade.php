55c75ef9f0e79460e317b0c44cfb8d8f37dbfb77
Erik Granström <erik.granstrom@helsingborg.se>
2022-01-17 11:33:10 +0100

    add heading field layout

---
+++ source/php/Module/EventForm/views/components/fields/heading.blade.php
@@ -0,0 +1,23 @@
+@php
+    $class = [];
+    $class[] = !$field['removeTopMargin'] ? 'u-margin__top--3' : '';
+    $class[] = !$field['removeBottomMargin'] ? 'u-margin__bottom--3' : '';
+    $class = trim(implode(' ', $class));
+@endphp
+
+<div class="{!! $class !!}">
+    @if (!empty($field['label']))
+        @typography([
+            'element' => $field['element'] ?? 'h2'
+            ])
+            {!! $field['label'] !!}
+        @endtypography
+    @endif
+    @if (!empty($field['description']))
+        @typography([
+            'element' => "p"
+            ])
+            {!! $field['description'] !!}
+        @endtypography
+    @endif
+</div>
