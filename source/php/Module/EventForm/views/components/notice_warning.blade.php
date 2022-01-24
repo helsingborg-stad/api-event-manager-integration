77b30aa5f7be3d713479361dbe4b59de5da42ae2
Freya <752642+faejr@users.noreply.github.com>
2022-01-24 11:14:33 +0100

    feat: display notices depending on form state

---
+++ source/php/Module/EventForm/views/components/notice_warning.blade.php
@@ -0,0 +1,20 @@
+{{-- Notice Warning--}}
+<div class="form-group submit-error hidden">
+    @notice([
+        'classList' => [
+            'u-display--none',
+            'event-submit__error'
+        ],
+        'type' => 'danger',
+        'message' => [
+            'text' => '#@ event-submit__error__text @#',
+            'size' => 'sm'
+        ],
+        'icon' => [
+            'name' => 'report',
+            'size' => 'md',
+            'color' => 'white'
+        ]
+    ])
+    @endnotice
+</div>
