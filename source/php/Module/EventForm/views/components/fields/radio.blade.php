df665da1a9af28240e7492e32bc096f07935a798
Erik Granström <erik.granstrom@helsingborg.se>
2022-01-25 11:36:37 +0100

    Add label and helper text

--- source/php/Module/EventForm/views/components/fields/radio.blade.php
+++ source/php/Module/EventForm/views/components/fields/radio.blade.php
@@ -1,14 +1,28 @@
-@foreach($field['options'] as $value => $label)
-    @option([
-        'type' => 'radio',
-        'checked' => $loop->index === 0,
-        'attributeList' => [
-            'data-id' => $option->id,
-            'name' => $field['name'],
-            'value' => $value,
-        ],
-        'classList' => ['u-display--inline-block'],
-        'label' => $label
-    ])
-    @endoption
-@endforeach
+<div class="c-field">
+    @if(!empty($field['label']))
+        <div class="c-field__label">
+            {!! $field['label'] !!}
+        </div>
+    @endif
+
+    @if(!empty($field['description']))
+        <div class="c-field__helper">
+            {!! $field['description'] !!}
+        </div>
+    @endif
+
+    @foreach($field['options'] as $value => $label)
+        @option([
+            'type' => 'radio',
+            'checked' => $loop->index === 0,
+            'attributeList' => [
+                'data-id' => $option->id,
+                'name' => $field['name'],
+                'value' => $value,
+            ],
+            'classList' => ['u-display--inline-block'],
+            'label' => $label
+        ])
+        @endoption
+    @endforeach
+</div>
