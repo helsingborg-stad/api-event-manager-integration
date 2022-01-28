31cc2bff69b85d6846d40bc38fbad41063c13c2c
Erik Granström <erik.granstrom@helsingborg.se>
2022-01-28 16:25:37 +0100

    add support for required attribute

--- source/php/Module/EventForm/views/components/fields/image.blade.php
+++ source/php/Module/EventForm/views/components/fields/image.blade.php
@@ -1,16 +1,9 @@
-
-<div class="c-field">
-    @include('components.fields.label', ['label' => $field['label']])
-
-    @imageinput([
-        'classList' => ['unlist'],
-        'name' => $field['name'],
-        'display' => 'area',
-        'multiple' => false,
-        'label' => $field['label'] ?? '',
-        'helperText' => $field['description'] ?? '',
-    ])
-    @endimageinput
-
-    @include('components.fields.helper', ['helper' => $field['description']])
-</div>
+@imageinput([
+    'classList' => ['unlist'],
+    'name' => $field['name'],
+    'required' => $field['required'] ?? false,
+    'display' => 'area',
+    'multiple' => false,
+    'label' => 'Choose images...'
+])
+@endimageinput
