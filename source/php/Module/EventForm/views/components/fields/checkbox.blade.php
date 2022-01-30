c98ef25b1e36999a07a0569b93f0c0eb323dde67
Erik Granström <erik.granstrom@helsingborg.se>
2022-01-30 16:08:45 +0100

    add support for required options

--- source/php/Module/EventForm/views/components/fields/checkbox.blade.php
+++ source/php/Module/EventForm/views/components/fields/checkbox.blade.php
@@ -1,20 +1,15 @@
-<div class="c-field">
-    @include('components.fields.label', ['label' => $field['label']])
-
-    @foreach($field['options'] as $value => $label)
-        @option([
-            'type' => 'checkbox',
-            'checked' => false,
-            'attributeList' => [
-                'data-id' => $option->id,
-                'name' => $field['name'],
-                'value' => $value,
-            ],
-            'classList' => ['u-display--inline-block'],
-            'label' => $label
-        ])
-        @endoption
-    @endforeach
-
-    @include('components.fields.helper', ['helper' => $field['description']])
-</div>
+@foreach($field['options'] as $value => $label)
+    @option([
+        'type' => 'checkbox',
+        'checked' => false,
+        'attributeList' => [
+            'data-id' => $option->id,
+            'name' => $field['name'],
+            'value' => $value,
+        ],
+        'classList' => ['u-display--inline-block'],
+        'label' => $label,
+        'required' => $field['required'] ?? false
+    ])
+    @endoption
+@endforeach
