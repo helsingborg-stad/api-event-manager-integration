2f5df5e026c697d49c87fbccceaf580b38d61d1e
Erik Granström <erik.granstrom@helsingborg.se>
2022-01-30 16:26:30 +0100

    add label and description

--- source/php/Module/EventForm/views/components/fields/image.blade.php
+++ source/php/Module/EventForm/views/components/fields/image.blade.php
@@ -1,11 +1,22 @@
-@imageinput([
-    'classList' => ['unlist'],
-    'name' => $field['name'],
-    'display' => 'area',
-    'multiple' => false,
-    'label' => 'Choose images...',
-    'maxFileSize' => $field['maxFileSize'] ?? 0,
-    'maxWidth' => $field['maxWidth'] ?? 0,
-    'maxHeight' => $field['maxHeight'] ?? 0,
-])
-@endimageinput
+<div class="c-field">
+    @if(!empty($field['label']))
+        <div class="c-field__label">
+            {!! $field['label'] !!}
+        </div>
+    @endif
+    
+    @imageinput([
+        'classList' => ['unlist'],
+        'name' => $field['name'],
+        'display' => 'area',
+        'multiple' => false,
+        'label' => 'Choose images...'
+    ])
+    @endimageinput
+
+    @if(!empty($field['description']))
+        <div class="c-field__helper">
+            {!! $field['description'] !!}
+        </div>
+    @endif
+</div>
