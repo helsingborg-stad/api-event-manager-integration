154ed1844414cf48a01192a0d1269efb6297b17d
Erik Granström <erik.granstrom@helsingborg.se>
2022-01-31 18:22:24 +0100

    add aspect ratio

--- source/php/Module/EventForm/views/components/fields/image.blade.php
+++ source/php/Module/EventForm/views/components/fields/image.blade.php
@@ -2,6 +2,7 @@
     'classList' => ['unlist'],
     'name' => $field['name'],
     'required' => $field['required'] ?? false,
+    'aspectRatio' => $field['aspectRatio'],
     'display' => 'area',
     'multiple' => false,
     'label' => 'Choose images...'
