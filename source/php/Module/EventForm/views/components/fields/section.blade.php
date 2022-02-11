d7abfc9d1c71eb28af8b855f6ebf4fcb67958dc3
Freya <752642+faejr@users.noreply.github.com>
2022-02-11 13:54:46 +0100

    fix: add id to sections

--- source/php/Module/EventForm/views/components/fields/section.blade.php
+++ source/php/Module/EventForm/views/components/fields/section.blade.php
@@ -1,4 +1,4 @@
-<section class="form-section">
+<section class="form-section" id="{{$field['name']}}">
     @if (!empty($field['label']) || !empty($field['description']))
         <header class="form-section__header form-section__body u-margin__bottom--3">
             @typography([
