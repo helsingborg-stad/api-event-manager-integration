02cc09ee479def0d3f4d9f45f35d35160469574c
Erik Granström <erik.granstrom@helsingborg.se>
2022-02-11 16:42:54 +0100

    move label and helper to component library

--- source/php/Module/EventForm/views/components/fields/image.blade.php
+++ source/php/Module/EventForm/views/components/fields/image.blade.php
@@ -1,18 +1,13 @@
-<div class="c-field">
-    @include('components.fields.label', ['label' => $field['label']])
-
-    @imageinput([
-        'classList' => ['unlist'],
-        'name' => $field['name'],
-        'display' => 'area',
-        'multiple' => false,
-        'label' => $field['label'] ?? '',
-        'helperText' => $field['description'] ?? '',
-        'maxFileSize' => $field['maxFileSize'] ?? 0,
-        'maxWidth' => $field['maxWidth'] ?? 0,
-        'maxHeight' => $field['maxHeight'] ?? 0,
-    ])
-    @endimageinput
-
-    @include('components.fields.helper', ['helper' => $field['description']])
-</div>
+@imageinput([
+    'classList' => ['unlist'],
+    'name' => $field['name'],
+    'display' => 'area',
+    'multiple' => false,
+    'required' => $field['required'],
+    'label' => $field['label'] ?? '',
+    'helperText' => $field['description'] ?? '',
+    'maxFileSize' => $field['maxFileSize'] ?? 0,
+    'maxWidth' => $field['maxWidth'] ?? 0,
+    'maxHeight' => $field['maxHeight'] ?? 0,
+])
+@endimageinput
