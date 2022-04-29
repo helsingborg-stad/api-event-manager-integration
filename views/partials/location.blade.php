1584cba5faf3131fa7dffd2743f91a4cab909d17
Erik Granström <erik.granstrom@helsingborg.se>
2022-04-29 10:38:35 +0200

    Add location widget

---
+++ views/partials/location.blade.php
@@ -0,0 +1,18 @@
+@card([])
+    <div class="c-card__body">
+        @include('partials.heading', ['heading' => get_the_title()])
+        
+        @include('partials.occasions')
+
+        @dump($event['occasion'])
+        
+        @if ($event['location'])
+            @typography([])
+                <strong>{{ _e('Location:', 'event-integration') }}</strong> {{ $event['location']['title'] }}
+            @endtypography
+        @endif
+
+        @include('partials.heading', ['heading' => __('Price', 'event-integration')])
+        @include('partials.price')
+    </div>
+@endcard
