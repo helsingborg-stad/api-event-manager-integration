859c26d50472ab2c07dbbc12a2db6dd1b2f577c7
Erik Granström <erik.granstrom@helsingborg.se>
2022-05-04 15:18:06 +0200

    Add widget for taxonomies

---
+++ views/partials/taxonomies.blade.php
@@ -0,0 +1,50 @@
+@if($groups|| $categories || $tags)
+    @card([])
+        <div class="c-card__body">
+            @if (!empty($groups) && is_array($groups))
+                @typography([
+                    'element'   => 'h2',
+                    'variant'   => 'h3',
+                    'classList' => ['c-card__heading', 'u-margin__top--2']
+                ])
+                    {{ $lang->groups }}
+                @endtypography
+                <ul>
+                    @foreach ($groups as $group)
+                        <li>{{ $group['name'] }}</li>
+                    @endforeach
+                </ul>
+            @endif
+
+            @if (!empty($categories) && is_array($categories))
+                @typography([
+                    'element'   => 'h2',
+                    'variant'   => 'h3',
+                    'classList' => ['c-card__heading', 'u-margin__top--2']
+                ])
+                    {{ $lang->categories }}
+                @endtypography
+                <ul>
+                    @foreach ($categories as $category)
+                        <li>{{ $category }}</li>
+                    @endforeach
+                </ul>
+            @endif
+
+            @if (!empty($tags) && is_array($tags))
+                @typography([
+                    'element'   => 'h2',
+                    'variant'   => 'h3',
+                    'classList' => ['c-card__heading', 'u-margin__top--2']
+                ])
+                    {{ $lang->tags }}
+                @endtypography
+                <ul>
+                    @foreach ($tags as $tag)
+                        <li>{{ $tag }}</li>
+                    @endforeach
+                </ul>
+            @endif
+        </div>
+    @endcard
+@endif
