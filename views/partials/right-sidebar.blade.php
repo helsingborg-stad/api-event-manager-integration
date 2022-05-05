a0e0646093d033c76cb99c1adb33c23f379d8b9e
Erik Granström <erik.granstrom@helsingborg.se>
2022-05-05 12:29:32 +0200

    Rename sidebar templaet

--- views/partials/right-sidebar.blade.php
+++
@@ -1,8 +0,0 @@
-@include('widgets.info')
-@include('widgets.membership-cards')
-@include('widgets.additional-ticket-types')
-@include('widgets.location')
-@include('widgets.organizer')
-@include('widgets.contacts')
-@include('widgets.taxonomies')
-@include('widgets.links')
