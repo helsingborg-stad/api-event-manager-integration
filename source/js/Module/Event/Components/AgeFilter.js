ada3d076e5e754dbb6e6c7566a30d4492725e302
NiclasNorin <niclas.norin@frontwalker.se>
2022-08-24 16:07:43 +0200

    Out of focus effect

--- source/js/Module/Event/Components/AgeFilter.js
+++ source/js/Module/Event/Components/AgeFilter.js
@@ -5,27 +5,47 @@ import { Input } from "@helsingborg-stad/hbg-react";
 const AgeFilter = ({ translation, ageRange, onAgeRangeChange, minValue, maxValue, onChange }) => {
     
   const minLimit = ageRange[0].id;
-  const maxLimit = ageRange.slice(-1).id;
+  const maxLimit = ageRange.slice(-1)[0].id;
+
+  console.log(ageRange);
 
   const minChanged = (e) => {
+    const inputField = e.target;
     const value = parseInt(e.target.value);
     const isMoreThenMax = value >= maxValue;
     const isLessThanMinLimit = minLimit > value;
     
     onChange({
-      min: isMoreThenMax ? maxValue - 1 : isLessThanMinLimit ? minLimit : value,
+      min: value,
       max: maxValue,
+    });
+
+    inputField.addEventListener('focusout', () => {
+      onChange({
+        min: isMoreThenMax ? maxValue - 1 : isLessThanMinLimit ? minLimit : value,
+        max: maxValue,
+      });
     });
   }
 
   const maxChanged = (e) => {
+    const inputField = e.target;
     const value = parseInt(e.target.value);
     const isLessOrEqualsMin = value <= minValue;
     const isMoreThenMaxLimit = value > maxLimit;
+    console.log(maxLimit, value);
 
     onChange({
       min: minValue,
-      max: isLessOrEqualsMin ? minValue + 1 : isMoreThenMaxLimit ? minValue +1 : value,
+      max: value,
+    });
+
+    inputField.addEventListener('focusout', () => {
+      console.log(isMoreThenMaxLimit);
+      onChange({
+        min: minValue,
+        max: isLessOrEqualsMin ? minValue + 1 : isMoreThenMaxLimit ? maxLimit : value,
+      });
     });
   }
 
