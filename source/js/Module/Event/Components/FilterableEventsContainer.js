6718f74d90e7ed9b9d1aca399e956af1d143b4d8
NiclasNorin <niclas.norin@frontwalker.se>
2022-08-26 10:09:52 +0200

    Fixed reset filters button

--- source/js/Module/Event/Components/FilterableEventsContainer.js
+++ source/js/Module/Event/Components/FilterableEventsContainer.js
@@ -112,14 +112,15 @@ class FilterableEventsContainer extends React.Component {
       tags,
       ageRange,
       translate,
+      ageRangeFilter
     } = this.state;
 
     const categoryIds = this.getTaxonomyIds(categories);
     const tagIds = this.getTaxonomyIds(tags);
     const ageRangeIds = this.getTaxonomyIds(ageRange);
     const params = this.loadQueryString();
-
-    categoryIds > 0 || tagIds > 0 ? this.setState({resetButton: true}) : this.setState({resetButton: false});
+   
+    categoryIds.length > 0 || tagIds.length > 0 || (ageRangeFilter.max - ageRangeFilter.min) !== ageRange.length ? this.setState({resetButton: true}) : this.setState({resetButton: false});
 
     // Set query parameters
     setQuery(
