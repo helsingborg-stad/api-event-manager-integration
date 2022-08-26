c55b6616662632e94d81324865323d3eb34b00ff
NiclasNorin <niclas.norin@frontwalker.se>
2022-08-26 11:53:41 +0200

    Fix age range bug

--- source/js/Module/Event/Components/AgeFilter.js
+++ source/js/Module/Event/Components/AgeFilter.js
@@ -4,22 +4,21 @@ import { Input } from "@helsingborg-stad/hbg-react";
 const AgeFilter = ({ translation, ageRange, minValue, maxValue, onChange }) => {
   const minLimit = ageRange[0].id;
   const maxLimit = ageRange.slice(-1)[0].id;
-
+  
   const minChanged = (e) => {
     const inputField = e.target;
     const value = parseInt(e.target.value);
     const isMoreThenMax = value >= maxValue;
     const isLessThanMinLimit = minLimit > value;
-    
-    onChange({
-      min: value,
-      max: maxValue,
-    });
+
+      onChange({
+        min: value,
+        max: maxValue
+      }); 
 
     inputField.addEventListener('focusout', () => {
       onChange({
         min: isMoreThenMax ? maxValue - 1 : isLessThanMinLimit ? minLimit : value,
-        max: maxValue,
       });
     });
   }
@@ -30,22 +29,21 @@ const AgeFilter = ({ translation, ageRange, minValue, maxValue, onChange }) => {
     const isLessOrEqualsMin = value <= minValue;
     const isMoreThenMaxLimit = value > maxLimit;
 
-    onChange({
-      min: minValue,
-      max: value,
-    });
+      onChange({
+        min: minValue,
+        max: value,
+     }); 
 
     inputField.addEventListener('focusout', () => {
       onChange({
         min: minValue,
-        max: isLessOrEqualsMin ? minValue + 1 : isMoreThenMaxLimit ? maxLimit : value,
+        max: isLessOrEqualsMin ? minValue + 1 : isMoreThenMaxLimit ? maxLimit + 1 : value,
       });
     });
   }
 
   return (
     <div>
-      {/* <span>{translation.selectAge}</span> */}
       <div className="age-filter-container o-grid u-flex-wrap--no-wrap">
         <Input         
           id="min"
