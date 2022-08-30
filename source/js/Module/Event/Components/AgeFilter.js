99885e1599b77b3abd0232dfff30da36bfe0c78a
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-08-30 13:19:07 +0200

    refactor range input logic

--- source/js/Module/Event/Components/AgeFilter.js
+++ source/js/Module/Event/Components/AgeFilter.js
@@ -4,46 +4,61 @@ import { Input } from "@helsingborg-stad/hbg-react";
 const AgeFilter = ({ translation, ageRange, minValue, maxValue, onChange }) => {
   const minLimit = ageRange[0].id;
   const maxLimit = ageRange.slice(-1)[0].id;
-  const maxInputField = document.querySelector('#max');
-  const minInputField = document.querySelector('#min');
   
-  const minChanged = (e) => {
-    const inputField = e.target;
-    const value = parseInt(e.target.value);
-    const isMoreThenMax = value >= maxValue;
-    const isLessThanMinLimit = minLimit > value;
+  const tryGetFirstNonNullValue = (args = {}, callbacks = []) => (
+    [...callbacks, () => ''].find(cb => cb(args) !== null)(args)
+  );
 
-      onChange({
-        min: value,
-        max: maxValue
-      }); 
+  const getMinValue = ({ min, max }) => (tryGetFirstNonNullValue(
+    {
+      min, 
+      max, 
+      isEmpty: isNaN(min),
+      isMoreThenMax: min >= max && !isNaN(max), 
+      isLessThanMinLimit: minLimit > min,
+    }, 
+    [
+      ({isEmpty})                 => isEmpty ? '' : null,
+      ({max, isMoreThenMax})      => isMoreThenMax ? max - 1 : null,
+      ({isLessThanMinLimit})      => isLessThanMinLimit ? minLimit : null,
+      ({min})                     => min,
+    ]
+  ));
 
-    inputField.addEventListener('focusout', () => {
-      onChange({
-        min: isMoreThenMax ? maxValue - 1 : isLessThanMinLimit ? minLimit : value,
-        max: parseInt(maxInputField.value)
-      });
-    });
-  }
+  const getMaxValue = ({ min, max }) => (tryGetFirstNonNullValue(
+    {
+      min, 
+      max, 
+      isEmpty : isNaN(max),
+      isLessOrEqualsMin: !isNaN(min) && max <= min, 
+      isMoreThenMaxLimit: max > maxLimit,
+    }, 
+    [
+      ({isEmpty})                 => isEmpty ? '' : null,
+      ({min, isLessOrEqualsMin})  => isLessOrEqualsMin ? min + 1 : null,
+      ({isMoreThenMaxLimit})      => isMoreThenMaxLimit ? maxLimit : null,
+      ({max})                     => max,
+    ]
+  ));
+  
+  const onChangeInputHandler = (isMin = true, onBlur = false) => (({ target : { value }}) => {
+    const isMax = !isMin;
+    const [ min, max ] = [
+      parseInt(isMin ? value : minValue),
+      parseInt(isMax ? value : maxValue)
+    ];
 
-  const maxChanged = (e) => {
-    const inputField = e.target;
-    const value = parseInt(e.target.value);
-    const isLessOrEqualsMin = value <= minValue;
-    const isMoreThenMaxLimit = value > maxLimit;
-
-      onChange({
-        min: minValue,
-        max: value,
-     }); 
-
-    inputField.addEventListener('focusout', () => {
-      onChange({
-        min: parseInt(minInputField.value),
-        max: isLessOrEqualsMin ? minValue + 1 : isMoreThenMaxLimit ? maxLimit + 1 : value,
-      });
-    });
-  }
+    onChange(onBlur 
+      ? {
+        min: isMin ? getMinValue({min, max}) : minValue,
+        max: isMax ? getMaxValue({min, max}) : maxValue,
+      }
+      : {
+        min: isMin ? min : minValue,
+        max: isMax ? max : maxValue,
+      }
+    );
+  });
 
   return (
     <div>
@@ -51,7 +66,8 @@ const AgeFilter = ({ translation, ageRange, minValue, maxValue, onChange }) => {
         <Input         
           id="min"
           name="min"
-          handleChange={minChanged}
+          handleChange={onChangeInputHandler(true)}
+          onBlur={onChangeInputHandler(true, true)}
           label="från ålder"
           type="number"
           value={minValue}
@@ -61,7 +77,8 @@ const AgeFilter = ({ translation, ageRange, minValue, maxValue, onChange }) => {
         <Input         
           id="max"
           name="max"
-          handleChange={maxChanged}
+          handleChange={onChangeInputHandler(false)}
+          onBlur={onChangeInputHandler(false, true)}
           label="till ålder"
           type="number"
           value={maxValue}
@@ -69,7 +86,6 @@ const AgeFilter = ({ translation, ageRange, minValue, maxValue, onChange }) => {
         />
       </div>
     </div>
-
   );
 };
 
