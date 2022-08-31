4463cd25b98843ad954a8a045a28dee275c7ace3
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-08-31 10:51:55 +0200

    format & simplify

--- source/js/Module/Event/Components/AgeFilter.js
+++ source/js/Module/Event/Components/AgeFilter.js
@@ -4,86 +4,91 @@ import { Input } from "@helsingborg-stad/hbg-react";
 const AgeFilter = ({ translation, ageRange, minValue, maxValue, onChange }) => {
   const minLimit = ageRange[0].id;
   const maxLimit = ageRange.slice(-1)[0].id;
-  
-  const tryGetFirstNonNullValue = (args = {}, callbacks = []) => (
-    [...callbacks, () => ''].find(cb => cb(args) !== null)(args)
-  );
 
-  const getMinValue = ({ min, max }) => (tryGetFirstNonNullValue(
-    {
-      min, 
-      max, 
-      isEmpty: isNaN(min),
-      isMoreThenMax: min >= max && !isNaN(max), 
-      isLessThanMinLimit: minLimit > min,
-    }, 
-    [
-      ({isEmpty})                 => isEmpty ? '' : null,
-      ({max, isMoreThenMax})      => isMoreThenMax ? max - 1 : null,
-      ({isLessThanMinLimit})      => isLessThanMinLimit ? minLimit : null,
-      ({min})                     => min,
-    ]
-  ));
+  const tryGetFirstNonNullValue = (args = {}, callbacks = []) =>
+    [...callbacks, () => ""].find((cb) => cb(args) !== null)(args);
 
-  const getMaxValue = ({ min, max }) => (tryGetFirstNonNullValue(
-    {
-      min, 
-      max, 
-      isEmpty : isNaN(max),
-      isLessOrEqualsMin: !isNaN(min) && max <= min, 
-      isMoreThenMaxLimit: max > maxLimit,
-    }, 
-    [
-      ({isEmpty})                 => isEmpty ? '' : null,
-      ({min, isLessOrEqualsMin})  => isLessOrEqualsMin ? min + 1 : null,
-      ({isMoreThenMaxLimit})      => isMoreThenMaxLimit ? maxLimit : null,
-      ({max})                     => max,
-    ]
-  ));
-  
-  const onChangeInputHandler = (isMin = true, onBlur = false) => (({ target : { value }}) => {
-    const isMax = !isMin;
-    const [ min, max ] = [
-      parseInt(isMin ? value : minValue),
-      parseInt(isMax ? value : maxValue)
-    ];
-
-    onChange(onBlur 
-      ? {
-        min: isMin ? getMinValue({min, max}) : minValue,
-        max: isMax ? getMaxValue({min, max}) : maxValue,
-      }
-      : {
-        min: isMin ? min : minValue,
-        max: isMax ? max : maxValue,
-      }
+  const getMinValue = ({ min, max }) =>
+    tryGetFirstNonNullValue(
+      {
+        min,
+        max,
+        isEmpty: isNaN(min),
+        isMoreThenMax: min >= max && !isNaN(max),
+        isLessThanMinLimit: minLimit > min,
+      },
+      [
+        ({ isEmpty }) => (isEmpty ? "" : null),
+        ({ max, isMoreThenMax }) => (isMoreThenMax ? max - 1 : null),
+        ({ isLessThanMinLimit }) => (isLessThanMinLimit ? minLimit : null),
+        ({ min }) => min,
+      ]
     );
-  });
+
+  const getMaxValue = ({ min, max }) =>
+    tryGetFirstNonNullValue(
+      {
+        min,
+        max,
+        isEmpty: isNaN(max),
+        isLessOrEqualsMin: !isNaN(min) && max <= min,
+        isMoreThenMaxLimit: max > maxLimit,
+      },
+      [
+        ({ isEmpty }) => (isEmpty ? "" : null),
+        ({ min, isLessOrEqualsMin }) => (isLessOrEqualsMin ? min + 1 : null),
+        ({ isMoreThenMaxLimit }) => (isMoreThenMaxLimit ? maxLimit : null),
+        ({ max }) => max,
+      ]
+    );
+
+  const onChangeInputHandler =
+    (isMin = true, onChangeCallback = (min, max) => {}) =>
+    ({ target: { value } }) => {
+      const isMax = !isMin;
+      const [min, max] = [
+        parseInt(isMin ? value : minValue),
+        parseInt(isMax ? value : maxValue),
+      ];
+
+      onChangeCallback({ min, max });
+    };
 
   return (
     <div>
-      <div className="age-filter-container o-grid u-flex-wrap--no-wrap">
-        <Input         
-          id="min"
-          name="min"
-          handleChange={onChangeInputHandler(true)}
-          onBlur={onChangeInputHandler(true, true)}
-          label="från ålder"
-          type="number"
-          value={minValue}
-          style={{width: "100%"}}
-        />
-        
-        <Input         
-          id="max"
-          name="max"
-          handleChange={onChangeInputHandler(false)}
-          onBlur={onChangeInputHandler(false, true)}
-          label="till ålder"
-          type="number"
-          value={maxValue}
-          style={{width: "100%"}}
-        />
+      <div className="age-filter-container o-grid o-grid--form u-flex-wrap--no-wrap">
+        <div className="o-grid-6">
+          <Input
+            id="min"
+            name="min"
+            handleChange={onChangeInputHandler(true, ({ min }) =>
+              onChange({ min, max })
+            )}
+            onBlur={onChangeInputHandler(true, ({ min, max }) =>
+              onChange({ min: getMinValue({ min, max }), max })
+            )}
+            label="från ålder"
+            type="number"
+            value={minValue}
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="o-grid-6">
+          <Input
+            id="max"
+            name="max"
+            handleChange={onChangeInputHandler(false, ({ min }) =>
+              onChange({ min, max })
+            )}
+            onBlur={onChangeInputHandler(false, ({ min, max }) =>
+              onChange({ min, max: getMaxValue({ min, max }) })
+            )}
+            label="till ålder"
+            type="number"
+            value={maxValue}
+            style={{ width: "100%" }}
+          />
+        </div>
       </div>
     </div>
   );
