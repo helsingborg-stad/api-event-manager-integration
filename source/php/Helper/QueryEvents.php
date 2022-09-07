25fc13157557bfab604bb8c8e033c38c87dc443c
Anna <anna@thehappyhipster.se>
2022-09-07 14:44:39 +0200

    When filtering events on min and/or max age also include all events that have neither min nor max ages set.
    (Corrected accidentally hardcoded table prefixes.)

--- source/php/Helper/QueryEvents.php
+++ source/php/Helper/QueryEvents.php
@@ -58,8 +58,10 @@ class QueryEvents
         $searchString = !empty($params['search_string']) ? $params['search_string'] : null;
         $hidePastEvents = !empty($params['hide_past_events']) ? $params['hide_past_events'] : false;
         $onlyTodaysDate = !empty($params['only_todays_date']) ? $params['only_todays_date'] : false;
+
         // Filter by age
-        $ageGroup = (!empty($params['age_group']) && is_array($params['age_group'])) ? $params['age_group'] : null;
+		$ageMin = ( '' !== $params['age_min'] ) ? (int) $params['age_min'] : false;
+		$ageMax = ( '' !== $params['age_max'] ) ? (int) $params['age_max'] : false;
 
         // Calculate offset
         $page = (!is_numeric($page)) ? 1 : $page;
@@ -71,16 +73,47 @@ class QueryEvents
         SELECT      *, $wpdb->posts.ID AS ID
         FROM        $wpdb->posts
         LEFT JOIN   $db_table ON ($wpdb->posts.ID = $db_table.event_id) ";
-        $query .= ($ageGroup) ? "LEFT JOIN $wpdb->postmeta age_from ON $wpdb->posts.ID = age_from.post_id " : '';
-        $query .= ($ageGroup) ? "LEFT JOIN $wpdb->postmeta age_to ON $wpdb->posts.ID = age_to.post_id " : '';
+
+		$query .= "LEFT JOIN $wpdb->postmeta AS mt1 ON ( $wpdb->posts.ID = mt1.post_id ) ";
+		$query .= "LEFT JOIN $wpdb->postmeta AS mt2 ON ( $wpdb->posts.ID = mt2.post_id AND mt2.meta_key = 'age_group_to' ) ";
+		$query .= "LEFT JOIN $wpdb->postmeta AS mt3 ON ( $wpdb->posts.ID = mt3.post_id AND mt3.meta_key = 'age_group_from' ) ";
+
         $query .= ($categories) ? "LEFT JOIN $wpdb->term_relationships term1 ON ($wpdb->posts.ID = term1.object_id) " : '';
         $query .= ($tags) ? "LEFT JOIN $wpdb->term_relationships term2 ON ($wpdb->posts.ID = term2.object_id) " : '';
         $query .= ($groups) ? "LEFT JOIN $wpdb->term_relationships term3 ON ($wpdb->posts.ID = term3.object_id) " : '';
         $query .= ($languageId) ? "LEFT JOIN $wpdb->term_relationships term4 ON ($wpdb->posts.ID = term4.object_id) " : '';
+
         $query .= "
         WHERE $wpdb->posts.post_type = %s
         AND $wpdb->posts.post_status = %s
         AND ($db_table.start_date BETWEEN %s AND %s OR $db_table.end_date BETWEEN %s AND %s) ";
+
+		if ( $ageMin ) {
+			$query .= "AND ( 
+				( 
+				  ( mt2.meta_key = 'age_group_from' AND mt2.meta_value <= $ageMin )
+				) 
+				OR 
+				( 
+				  mt2.post_id IS NULL 
+				  AND 
+				  mt3.post_id IS NULL
+				)
+			)";
+		}
+		if ( $ageMax ) {
+			$query .= "AND ( 
+				( 
+				  ( mt3.meta_key = 'age_group_to' AND mt3.meta_value >= $ageMax )
+				) 
+				OR 
+				( 
+				  mt2.post_id IS NULL 
+				  AND 
+				  mt3.post_id IS NULL
+				)
+			)";
+		}
 
         if ($hidePastEvents){
             $query .= " AND $db_table.end_date > '". date('Y-m-d H:i:s')."' ";
@@ -88,21 +121,6 @@ class QueryEvents
 
         if($onlyTodaysDate) {
             $query .= " AND $db_table.end_date <= '". date('Y-m-d H:i:s', strtotime('tomorrow - 1 second'))."' ";
-        }
-
-        if ($ageGroup) {
-            $query .= "AND (age_from.meta_key = 'age_group_from' AND age_to.meta_key = 'age_group_to' AND (";
-
-            $numItems = count($ageGroup);
-            $i = 0;
-            foreach ($ageGroup as $key => $age) {
-                if (++$i === $numItems) {
-                    $query .= "{$age} BETWEEN age_from.meta_value AND age_to.meta_value";
-                } else {
-                    $query .= "{$age} BETWEEN age_from.meta_value AND age_to.meta_value OR ";
-                }
-            }
-            $query .= ")) ";
         }
 
         $query .= ($searchString) ? "AND (($wpdb->posts.post_title LIKE %s) OR ($wpdb->posts.post_content LIKE %s))" : '';
