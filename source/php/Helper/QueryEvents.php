82c40bff7c42208dfc5bd57f6b1592e4d4adc2ef
Anna Johansson <anna@thehappyhipster.se>
2022-09-20 16:48:57 +0200

    Set NaN to empty string

--- source/php/Helper/QueryEvents.php
+++ source/php/Helper/QueryEvents.php
@@ -60,8 +60,8 @@ class QueryEvents
         $onlyTodaysDate = !empty($params['only_todays_date']) ? $params['only_todays_date'] : false;
 
         // Filter by age
-        $ageMin = ('' !== $params['age_min']) ? (int) $params['age_min'] : false;
-        $ageMax = ('' !== $params['age_max']) ? (int) $params['age_max'] : false;
+        $ageMin = ('' !== $params['age_min'] ) ? (int) $params['age_min'] : false;
+        $ageMax = ('' !== $params['age_max'] ) ? (int) $params['age_max'] : false;
 
         // Calculate offset
         $page = (!is_numeric($page)) ? 1 : $page;
@@ -74,9 +74,12 @@ class QueryEvents
         FROM        $wpdb->posts
         LEFT JOIN   $db_table ON ($wpdb->posts.ID = $db_table.event_id) ";
 
-        $query .= "LEFT JOIN $wpdb->postmeta AS meta2 ON ( $wpdb->posts.ID = meta2.post_id AND meta2.meta_key = 'age_group_from' ) ";
-        $query .= "LEFT JOIN $wpdb->postmeta AS meta3 ON ( $wpdb->posts.ID = meta3.post_id AND meta3.meta_key = 'age_group_to' ) ";
-
+        if ($ageMin) {
+            $query .= "LEFT JOIN $wpdb->postmeta AS meta2 ON ( $wpdb->posts.ID = meta2.post_id AND meta2.meta_key = 'age_group_from' ) ";
+        }
+        if ($ageMax) {
+            $query .= "LEFT JOIN $wpdb->postmeta AS meta3 ON ( $wpdb->posts.ID = meta3.post_id AND meta3.meta_key = 'age_group_to' ) ";
+        }
         $query .= ($categories) ? "LEFT JOIN $wpdb->term_relationships term1 ON ($wpdb->posts.ID = term1.object_id) " : '';
         $query .= ($tags) ? "LEFT JOIN $wpdb->term_relationships term2 ON ($wpdb->posts.ID = term2.object_id) " : '';
         $query .= ($groups) ? "LEFT JOIN $wpdb->term_relationships term3 ON ($wpdb->posts.ID = term3.object_id) " : '';
@@ -87,12 +90,12 @@ class QueryEvents
         AND $wpdb->posts.post_status = %s
         AND ($db_table.start_date BETWEEN %s AND %s OR $db_table.end_date BETWEEN %s AND %s) ";
 
-		if ($ageMin) {
+        if ($ageMin) {
             $query .= "AND ( meta2.meta_value <= $ageMin OR meta2.meta_value IS NULL ) ";
-        } 
-		if ($ageMax) {
+        }
+        if ($ageMax) {
             $query .= "AND ( meta3.meta_value >= $ageMax OR meta3.meta_value IS NULL ) ";
-        } 
+        }
 
         if ($hidePastEvents) {
             $query .= "AND $db_table.end_date > '". date('Y-m-d H:i:s')."' ";
