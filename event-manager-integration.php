fd245dcb9c1d351d5b309a70b6cb2558dc5384ed
GitHub Action <>
2024-11-19 09:18:11 +0000

    build: update package file versions to 2.0.23

--- event-manager-integration.php
+++ event-manager-integration.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Event Manager Integration
  * Plugin URI:        https://github.com/helsingborg-stad/api-event-manager-integration
  * Description:       Integrate and display events from Event Manager API.
- * Version: 2.0.22
+ * Version: 2.0.23
  * Author:            Jonatan Hanson, Sebastian Thulin
  * Author URI:        http://www.helsingborg.se
  * License:           MIT
