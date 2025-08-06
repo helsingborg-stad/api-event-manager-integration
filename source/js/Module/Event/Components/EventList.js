63a89487113d03c5a9d8c03468b3bd74a21643e2
Michael Claesson <michael.claesson@consid.se>
2025-08-06 19:07:06 +0200

    WIP

--- source/js/Module/Event/Components/EventList.js
+++ source/js/Module/Event/Components/EventList.js
@@ -3,22 +3,72 @@ import { v4 as uuidv4 } from 'uuid';
 import EventItem from './EventItem';
 
 class EventList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentIndex: 0,
+    };
+    this.itemRefs = [];
+    this.containerRef = React.createRef();
+  }
+
+  scrollToItem = (index) => {
+    const itemNode = this.itemRefs[index];
+    const container = this.containerRef.current;
+    if (itemNode && container) {
+      const scrollLeft = itemNode.offsetLeft - container.offsetLeft;
+      container.scrollTo({ left: scrollLeft, behavior: 'smooth' });
+    }
+  };
+
+  handlePrev = () => {
+    this.setState(
+      prevState => ({ currentIndex: Math.max(prevState.currentIndex - 1, 0) }),
+      () => this.scrollToItem(this.state.currentIndex)
+    );
+  };
+
+  handleNext = () => {
+    const { items } = this.props;
+    this.setState(
+      prevState => ({ currentIndex: Math.min(prevState.currentIndex + 1, items.length - 1) }),
+      () => this.scrollToItem(this.state.currentIndex)
+    );
+  };
+
   render() {
-    const { items, gridColumn, displayFields, cardStyle, imageRatio } = this.props;
+    const { items, gridColumn, displayFields, cardStyle, imageRatio, mobileSlider, translation } = this.props;
+    const { currentIndex } = this.state;
+    this.itemRefs = [];
     return (
-      <div className="o-grid">
-        {items.map(event => (
-          <div className={gridColumn ?? `o-grid-12 o-grid-4@md`}>
-            <EventItem
-              cardStyle={cardStyle}
+      <>
+        {mobileSlider && 
+          <ul>
+            <li className="prev" aria-label={translation.prev}>
+              <button onClick={this.handlePrev} disabled={currentIndex === 0}>&lt;</button>
+            </li>
+            <li className="next" aria-label={translation.next}>
+              <button onClick={this.handleNext} disabled={currentIndex === items.length - 1}>&gt;</button>
+            </li>
+          </ul>
+        }
+        <div className="o-grid" ref={this.containerRef}>
+          {items.map((event, idx) => (
+            <div
+              className={gridColumn ?? `o-grid-12 o-grid-4@md`}
               key={uuidv4()}
-              event={event}
-              displayFields={displayFields}
-              imageRatio={imageRatio}
-            />
-          </div>
-        ))}
-      </div>
+              ref={el => this.itemRefs[idx] = el}
+            >
+              <EventItem
+                cardStyle={cardStyle}
+                event={event}
+                displayFields={displayFields}
+                imageRatio={imageRatio}
+              />
+            </div>
+          ))}
+        </div>
+      </>
     );
   }
 }
@@ -27,6 +77,7 @@ EventList.propTypes = {
   displayFields: PropTypes.array,
   gridColumn: PropTypes.string,
   items: PropTypes.array.isRequired,
+  mobileSlider: PropTypes.bool,
 };
 
 export default EventList;
