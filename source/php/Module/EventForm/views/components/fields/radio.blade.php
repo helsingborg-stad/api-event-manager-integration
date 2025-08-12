ef76a60c533a1e2a6937f372f0802d9b98cb1479
NiclasNorin <niclasn@outlook.com>
2025-08-12 10:53:15 +0200

    fix: correcting option component parameters

--- source/php/Module/EventForm/views/components/fields/radio.blade.php
+++ source/php/Module/EventForm/views/components/fields/radio.blade.php
@@ -5,10 +5,10 @@
         @option([
             'type' => 'radio',
             'checked' => $loop->index === 0,
+            'value' => $value,
+            'name' => $field['name'],
             'attributeList' => [
-                'data-id' => !empty($option->id) ? $option->id : uniqid(),
-                'name' => $field['name'],
-                'value' => $value,
+                'data-id' => !empty($option->id) ? $option->id : uniqid()
             ],
             'classList' => ['u-display--inline-block'],
             'label' => $label,
