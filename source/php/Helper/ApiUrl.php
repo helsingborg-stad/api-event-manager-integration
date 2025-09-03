effac20a375ee68ddf6a81464ff5993128c5ac1e
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-09-03 16:51:10 +0200

    fix: do not check against all groups setting before import

--- source/php/Helper/ApiUrl.php
+++ source/php/Helper/ApiUrl.php
@@ -18,16 +18,24 @@ class ApiUrl
             WHERE       $wpdb->posts.post_type = 'event'
             AND         $wpdb->posts.post_status = 'publish'
             ORDER BY    $dbTable.start_date
-            ASC LIMIT 1", ARRAY_A);
+            ASC LIMIT 1", ARRAY_A
+        );
 
-        $fromDate = (is_array($occasion) && isset($occasion[0]['start_date']) && strtotime($occasion[0]['start_date']) < strtotime('now')) ? date('Y-m-d', strtotime($occasion[0]['start_date'])) : date('Y-m-d');
+        $fromDate = (is_array($occasion) && isset($occasion[0]['start_date']) && strtotime($occasion[0]['start_date']) < strtotime('now'))
+            ? date('Y-m-d', strtotime($occasion[0]['start_date']))
+            : date('Y-m-d');
+
         $daysAhead = !empty(get_field('days_ahead', 'options')) ? absint(get_field('days_ahead', 'options')) : 30;
         $toDate = date('Y-m-d', strtotime("midnight now + {$daysAhead} days"));
         $importLocation = get_field('event_import_from_location', 'option');
+
         // Get nearby events from location
         $point = get_field('event_import_geographic', 'option');
         $latLng = ($point && $importLocation == 'point') ? '&latlng=' . $point['lat'] . ',' . $point['lng'] : '';
-        $distance = (get_field('event_geographic_distance', 'option') && $importLocation == 'point') ? '&distance=' . get_field('event_geographic_distance', 'option') : '';
+        $distance = (get_field('event_geographic_distance', 'option') && $importLocation == 'point')
+            ? '&distance=' . get_field('event_geographic_distance', 'option')
+            : '';
+
         // Get events within area
         $areaLatLng = '';
         $areaPoints = get_option('event_import_area', 'option');
@@ -38,17 +46,13 @@ class ApiUrl
                 }
             }
         }
+
         // Filter by selected groups
         $groups = '';
         $selectedGroups = json_decode(json_encode(get_field('event_filter_group', 'option')), true);
         if (is_array($selectedGroups) && !empty($selectedGroups)) {
-            $selectedGroups = array_column($selectedGroups, 'slug');
-            $allGroups = array_column(get_option('event_user_groups', array()), 'id', 'slug');
-            // Create array with selected group IDs
-            $groupIds = array_map(function ($a) use ($allGroups) {
-                return isset($allGroups[$a]) ? $allGroups[$a] : null;
-            }, $selectedGroups);
-            $groups = '&group-id=' . implode(',', array_filter($groupIds));
+            $groupValues = array_column($selectedGroups, 'slug');
+            $groups = '&group-id=' . implode(',', array_filter($groupValues));
         }
 
         // Adds internal event parameter
